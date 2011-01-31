bd27612107a909f002569f249756b07c276ec8cb
jtowle <jeff@urbanairship.com>
2011-01-31 11:47:08 -0800

    Updated copyright notice.

--- Airship/Library/PushLib/UAPush.h
+++ Airship/Library/PushLib/UAPush.h
@@ -1,5 +1,5 @@
 /*
- Copyright 2009-2010 Urban Airship Inc. All rights reserved.
+ Copyright 2009-2011 Urban Airship Inc. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
