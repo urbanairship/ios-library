c5e8d0b188cdb181cd8e21a9dd05743d1757cbb2
jtowle <jeff@urbanairship.com>
2011-09-27 15:21:11 -0700

    Appledoc publication script updates.

--- fabfile.py
+++ fabfile.py
@@ -14,12 +14,12 @@ def build_docs():
 
 def upload_docs():
     print "Creating tar of docs..."
-    fab.local("tar czf ios_inbox_docs.tgz -C docs/html .")
-    print "Done! Uploading now..(except not actually)"
-    #fab.put("ios_inbox_docs.tgz", "/django/docs/wherever_this_goes/ios_inbox_docs.tgz")
-    print "Done! Untar'ing docs remotely..(except not actually)"
-    #fab.run("cd /django/docs/wherever_this_goes/ && tar xzf ios_inbox_docs.tgz && rm ios_inbox_docs.tgz")
-    fab.local("rm ios_inbox_docs.tgz")
+    fab.local("tar czf ios_docs.tgz -C docs/html .")
+    print "Done! Uploading now..."
+    fab.put("ios_docs.tgz", "/django/docs/ios_lib/ios_docs.tgz")
+    print "Done! Untar'ing docs remotely..."
+    fab.run("cd /django/docs/ios_lib/ && tar xzf ios_docs.tgz && rm ios_docs.tgz")
+    fab.local("rm ios_docs.tgz")
     print "Thanks, have a nice day!"
 
 def build_and_upload_docs():
