aef4289c6c8bbcde267150d9ffede8122c7af1af
Matt Hooge <mhooge@gmail.com>
2012-08-01 11:40:25 -0700

    LIB-392 Xcode4.4 errors
    - fixed string format specifier warnings
    - fixed dangling if/else statements, a fiesta of {
    - changed UASubscriptionObserver logging where a value was cast as an int, but formatted as an object.

--- Airship/External/UA_fmdb/UA_FMDatabase.m
+++ Airship/External/UA_fmdb/UA_FMDatabase.m
@@ -850,7 +850,7 @@ return ret;
 }
 
 - (NSString*) description {
-    return [NSString stringWithFormat:@"%@ %d hit(s) for query %@", [super description], useCount, query];
+    return [NSString stringWithFormat:@"%@ %lu hit(s) for query %@", [super description], useCount, query];
 }
 
 @end
