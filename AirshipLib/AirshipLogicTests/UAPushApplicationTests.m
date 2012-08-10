4ddbf845cc902dd5605d544bceba736e79d46456
Matt Hooge <mhooge@gmail.com>
2012-08-10 16:06:32 -0700

    1.3.2 Remove failed test
    - removed failed tests for 1.3.2, schedule work for later date

--- AirshipLib/AirshipLogicTests/UAPushApplicationTests.m
+++ AirshipLib/AirshipLogicTests/UAPushApplicationTests.m
@@ -271,48 +271,6 @@ static BOOL messageReceived = NO;
     STAssertFalse(push.isRegistering, @"isRegistering should be NO");
 }
 
-//- (void)testUpdateRegistrationWhenPushEnabledAndCacheNotStale {
-//    push.isRegistering = NO;
-//    push.pushEnabled = YES;
-//    NSDictionary *cache = @{ @"key" : @"value" };
-//    push.registrationPayloadCache = cache;
-//    push.pushEnabledPayloadCache = YES;
-//    id mockPush = [OCMockObject partialMockForObject:push];
-//    [[[mockPush stub] andReturn:cache] registrationPayload];
-//    [[mockPush reject] requestToRegisterDeviceTokenWithInfo:OCMOCK_ANY];
-//    [push updateRegistration];
-//    
-//}
-
-//- (void)testUpdateRegistrationWhenPushEnabledAndCacheIsStale {
-//    push.isRegistering = NO;
-//    push.pushEnabled = YES;
-//    push.registrationPayloadCache = @{ @"key" : @"StaleValue" };
-//    id mockRequest = [OCMockObject niceMockForClass:[UA_ASIHTTPRequest class]];
-//    [[mockRequest expect] startAsynchronous];
-//    id mockPush = [OCMockObject partialMockForObject:push];
-//    [[[mockPush stub] andReturn:mockRequest] requestToRegisterDeviceTokenWithInfo:OCMOCK_ANY];
-//    [push updateRegistration];
-//    [mockRequest verify];
-//}
-
-//- (void)testUpdateRegistrationwhenPushNotEnabledAndCacheIsStale {
-//    push.isRegistering = NO;
-//    push.pushEnabled = NO;
-//    push.registrationPayloadCache = @{ @"key" : @"ADifferentStaleValue" };
-//    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:UAPushNeedsUnregistering];
-//    id mockRequest = [OCMockObject niceMockForClass:[UA_ASIHTTPRequest class]];
-//    [[mockRequest expect] startAsynchronous];
-//    id mockPush = [OCMockObject partialMockForObject:push];
-//    [[[mockPush stub] andReturn:mockRequest] requestToDeleteDeviceToken];
-//    [push updateRegistration];
-//    [mockRequest verify];
-//    // Throw in extra check for needs unregistering flag
-//    mockPush = [OCMockObject partialMockForObject:push];
-//    [[mockPush reject] requestToDeleteDeviceToken];
-//    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:UAPushNeedsUnregistering];
-//    [push updateRegistration];
-//}
 /////////////////////////
 
 - (void)testRegisterForRemoteNotificationTypes {
@@ -493,22 +451,6 @@ static BOOL messageReceived = NO;
 
 #pragma mark -
 #pragma mark UA API Registration callbacks
-
-//- (void)testCacheHasChangedComparedToUserInfo {
-//    // Rig the cached value with matched settings
-//    push.registrationPayloadCache = [push registrationPayload];
-//    push.pushEnabled = NO;
-//    push.pushEnabledPayloadCache = NO;
-//    // Get a user info object that would be attached to a UA_HTTPRequest, use the registrationPayload again
-//    NSDictionary *userInfoDictionary = [push cacheForRequestUserInfoDictionaryUsing:[push registrationPayload]];
-//    STAssertFalse([push cacheHasChangedComparedToUserInfo:userInfoDictionary], @"chacheHasChanged should be NO");
-//    push.pushEnabled = YES;
-//    STAssertTrue([push cacheHasChangedComparedToUserInfo:userInfoDictionary], @"cacheHasChanged should be YES");
-//    push.pushEnabled = NO;
-//    userInfoDictionary = [push cacheForRequestUserInfoDictionaryUsing:[NSDictionary dictionaryWithObject:@"cat" forKey:@"key"]];
-//    STAssertTrue([push cacheHasChangedComparedToUserInfo:userInfoDictionary], @"cacheHasChanged should be YES");
-//    
-//}
 
 // This test covers the basic error case, the workflow where the response from the server is NOT a 500
 - (void)testRegisterDeviceTokenFailed {
