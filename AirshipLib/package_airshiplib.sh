c57d1f6aa7214627083412634c227046ef49dbc4
Matt Hooge <mhooge@gmail.com>
2012-09-17 13:49:32 -0700

    Release updates
    - changed path in package_airship.sh to output to Release

--- AirshipLib/package_airshiplib.sh
+++ AirshipLib/package_airshiplib.sh
@@ -23,8 +23,8 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#TODO: rename this
-buildConfig="distribution_package"
+# TODO: make this configurable?
+buildConfig="Release"
 srcRoot=$(pwd)
 
 srcPath="${srcRoot}/../Airship"
