2e564a1ed2bc8f2067ede4314043549b29cfdc36
jtowle <jeff@urbanairship.com>
2013-06-04 10:57:30 -0700

    Updated headers.

--- PushSample/FunctionalTests/Classes/KIFTestScenario+UAAdditions.m
+++ PushSample/FunctionalTests/Classes/KIFTestScenario+UAAdditions.m
@@ -1,10 +1,27 @@
-//
-//  KIFTestScenario+UAAdditions.m
-//  PushSampleLib
-//
-//  Created by Jeff Towle on 6/1/13.
-//
-//
+/*
+ Copyright 2009-2013 Urban Airship Inc. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binaryform must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided withthe distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "KIFTestScenario+UAAdditions.h"
 
@@ -21,8 +38,6 @@
 
 + (id)scenarioToEnablePush {
     KIFTestScenario *scenario = [KIFTestScenario scenarioWithDescription:@"Test that push is can be enabled in the settings screen."];
-    //[scenario addStep:[KIFTestStep stepToReset]];
-    //[scenario addStepsFromArray:[KIFTestStep stepsToGoToLoginPage]];
     [scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"Push Settings"]];
     [scenario addStep:[KIFTestStep stepToSetOn:YES forSwitchWithAccessibilityLabel:@"Push Notifications Enabled"]];
     [scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"Done" traits:UIAccessibilityTraitButton]];
@@ -115,6 +130,9 @@
         [scenario addStep:
             [KIFTestStep stepToTapViewWithAccessibilityLabel:[NSString stringWithFormat:@"Delete %@", tag]
                                                       traits:UIAccessibilityTraitButton]];
+        [scenario addStep:
+         [KIFTestStep stepToTapViewWithAccessibilityLabel:[NSString stringWithFormat:@"Confirm Deletion for %@", tag]
+                                                   traits:UIAccessibilityTraitButton]];
     }
 
     [scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"Add" traits:UIAccessibilityTraitButton]];
@@ -149,6 +167,5 @@
     
     return scenario;
 }
-
 
 @end
