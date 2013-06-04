3dddf3efd95980abd252878eac989b7a5ff1d275
marc <marc@urbanairship.com>
2013-06-04 14:25:09 -0700

    UAUserAPIClient

--- Airship/Common/UAUserAPIClient.h
+++ Airship/Common/UAUserAPIClient.h
@@ -1,13 +1,15 @@
-//
-//  UAUserAPIClient.h
-//  AirshipLib
-//
-//  Created by Marc Sciglimpaglia on 5/24/13.
-//
-//
 
 #import <Foundation/Foundation.h>
+#import "UAUserData.h"
+#import "UAHTTPConnection.h"
+
+typedef void (^UAUserAPIClientCreateSuccessBlock)(UAUserData *data, NSString *deviceToken);
+typedef void (^UAUserAPIClientUpdateSuccessBlock)(NSString *deviceToken);
+typedef void (^UAUserAPIClientFailureBlock)(UAHTTPRequest *request);
 
 @interface UAUserAPIClient : NSObject
+
+- (void)createUserOnSuccess:(UAUserAPIClientCreateSuccessBlock)successBlock onFailure:(UAUserAPIClientFailureBlock)failureBlock;
+- (void)updateDeviceToken:(NSString *)deviceToken forUsername:(NSString *)username onSuccess:(UAUserAPIClientUpdateSuccessBlock)successBlock onFailure:(UAUserAPIClientFailureBlock)failureBlock;
 
 @end
