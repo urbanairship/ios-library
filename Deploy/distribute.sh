2e7e595f9d5a3a6f9f0e75302bb1faa18f22a115
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-06-24 14:05:54 -0700

    Update copyright year

--- Deploy/distribute.sh
+++ Deploy/distribute.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2009-2012 Urban Airship Inc. All rights reserved.
+# Copyright 2009-2013 Urban Airship Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -22,7 +22,6 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 
 outputPath="./output"
 originalPath=`pwd`
