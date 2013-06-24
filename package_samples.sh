006727d91ca3a1bddf438a5d30b607abe6a41e88
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-06-24 09:56:23 -0700

    Package samples and library together

--- package_samples.sh
+++ package_samples.sh
@@ -23,46 +23,103 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-for sample in InboxSample PushSample StoreFrontSample SubscriptionSample; do
-	if [ ! -d "$sample" ]; then
+# TODO: make this configurable?
+buildConfig="Release"
+srcRoot=$(pwd)
+destPath="${srcRoot}/${buildConfig}"
+
+for expected in InboxSample PushSample Airship; do
+	if [ ! -d "$expected" ]; then
 		echo "You need to run this script under root directory."
 		exit -1
 	fi
 done
 
-rm -rf Release
-mkdir Release
-cp -R *Sample Release
+# Remove any old deploy
+rm -rf "${destPath}"
+mkdir -p "${destPath}"
 
-cd Release
-rm -rf `find . -name "build"`
-rm -rf `find . -name "*SampleLib.xcodeproj"`
-rm -rf `find . -name "*Tests"`
-rm -rf `find . -name "*Test*.plist"`
-rm -rf `find . -name "*Test*.pch"`
-rm -rf `find . -name "*.orig" `
+# Prepare Airship
+#################
+rm -rf distribution_binaries
+./buildTarget.sh AirshipLib
+./update_library_reference.sh
+./package_airshiplib.sh
 
-#delete user-specific xcode files
-rm -rf `find . -name "*.mode1v3" `
-rm -rf `find . -name "*.pbxuser" `
-rm -rf `find . -name "*.perspective*" `
-rm -rf `find . -name "xcuserdata" `
+airshipPath="${srcRoot}/Airship"
 
-#delete the testing config plist
-rm -rf `find . -name "AirshipDevelopment.plist" `
+echo "cp -R \"${airshipPath}\" \"${srcRoot}/${buildConfig}\""
+cp -R "${airshipPath}" "${srcRoot}/${buildConfig}"
+
+# Remove all non .h files from /Library and /Common
+# Remove all non UA_ items & dirs from Airship/External
+find "${destPath}/Airship/Library" \! -name "*.h" -type f -delete
+find "${destPath}/Airship/Common" \! -name "*.h" -type f -delete
+
+# Delete internal test headers
+rm -rf `find "${destPath}/Airship" -name "*+Internal.h" `
+
+find "${destPath}/Airship/External" \! '(' -name "UA_*.h" -o -name "UA_" ')' -type f -delete
+find "${destPath}/Airship/External" -type d -empty -delete
+rm -rf "${destPath}/Airship/TestSamples"
+rm -rf "${destPath}/Airship/Test"
+
+# Remove the Appledoc documenation settings from the distribution
+rm "${destPath}/Airship/AppledocSettings.plist"
+
+find "${destPath}/Airship" -name "*.orig" -delete
+
+# Copy LICENSE, README and CHANGELOG
+cp "${airshipPath}/../CHANGELOG" "${destPath}/Airship"
+cp "${airshipPath}/../README.rst" "${destPath}/Airship"
+cp "${airshipPath}/../LICENSE" "${destPath}/Airship"
 
 
-#rename packages for distribution
-mv InboxSample RichPushSample
+# Prepare Samples
+#################
+
+cp -R InboxSample "${destPath}/RichPushSample" # Rename InboxSample to RichPushSample
+cp -R PushSample "${destPath}"
 
 # copy the sample plist into place
 for sample in RichPushSample PushSample; do
-    cp ../CHANGELOG $sample
-    cp ../LICENSE $sample
-    cp ../README.rst $sample
-    mv -f $sample/AirshipConfig.plist.sample $sample/AirshipConfig.plist
-    zip -r $sample-latest.zip $sample
+	samplePath="${destPath}/${sample}"
+
+	rm -rf `find ${samplePath} -name "build"`
+	rm -rf `find ${samplePath} -name "*SampleLib.xcodeproj"`
+	rm -rf `find ${samplePath} -name "*Tests"`
+	rm -rf `find ${samplePath} -name "*Test*.plist"`
+	rm -rf `find ${samplePath} -name "*Test*.pch"`
+	rm -rf `find ${samplePath} -name "*.orig" `
+
+	#delete user-specific xcode files
+	rm -rf `find ${samplePath} -name "*.mode1v3" `
+	rm -rf `find ${samplePath} -name "*.pbxuser" `
+	rm -rf `find ${samplePath} -name "*.perspective*" `
+	rm -rf `find ${samplePath} -name "xcuserdata" `
+
+	#delete the testing config plist
+	rm -rf `find ${samplePath} -name "AirshipDevelopment.plist" `
+
+    cp CHANGELOG $samplePath
+    cp LICENSE $samplePath
+    cp README.rst $samplePath
+    mv -f $samplePath/AirshipConfig.plist.sample $samplePath/AirshipConfig.plist
 done
+
+# Package Release
+#################
+
+#TODO: pull out version number from xcodeproject and create both files.
+cd $destPath
+
+for package in RichPushSample PushSample Airship; do
+	zip -r libUAirship-latest.zip $package
+done
+
+cd ..
+
+
 
 
 
