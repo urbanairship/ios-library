708fac4332c1eef125ced08f169941d6eb1ee719
marc <marc@urbanairship.com>
2013-06-26 12:51:11 -0700

    appledox

--- Airship/Library/InboxLib/NSDictionary+RichPushData.h
+++ Airship/Library/InboxLib/NSDictionary+RichPushData.h
@@ -3,8 +3,15 @@
 
 typedef void (^UAInboxMessageIDBlock)(NSString *messageID);
 
+/**
+ * Helper category extensions for parsing Rich Push data from notification dictionaries.
+ */
 @interface NSDictionary(RichPushData)
 
+/**
+ *  Retrieves a rich push message ID from a notification dictionary, executing
+ *  The supplied block with the ID as an argument if it is found.
+ */
 - (void)getRichPushMessageIDWithAction:(UAInboxMessageIDBlock)actionBlock;
 
 @end
