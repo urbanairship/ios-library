750ec2308fdaf9250aa2e81c6beb184b32bff32c
marc <marc@urbanairship.com>
2013-06-26 16:58:31 -0700

    category -> UAInboxUtils

---
+++ Airship/Library/InboxLib/UAInboxUtils.h
@@ -0,0 +1,13 @@
+//
+//  UAInboxUtils.h
+//  AirshipLib
+//
+//  Created by Marc Sciglimpaglia on 6/26/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UAInboxUtils : NSObject
+
+@end
---
+++ Airship/Library/InboxLib/UAInboxUtils.m
@@ -0,0 +1,13 @@
+//
+//  UAInboxUtils.m
+//  AirshipLib
+//
+//  Created by Marc Sciglimpaglia on 6/26/13.
+//
+//
+
+#import "UAInboxUtils.h"
+
+@implementation UAInboxUtils
+
+@end
