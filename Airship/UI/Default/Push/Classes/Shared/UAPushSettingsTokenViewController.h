5c31d1632782c8c96d15c2c7af3ff473c1e09020
marc <marc@urbanairship.com>
2013-07-02 11:39:33 -0700

    autosynthesize UAPushSettingsTokenViewController

--- Airship/UI/Default/Push/Classes/Shared/UAPushSettingsTokenViewController.h
+++ Airship/UI/Default/Push/Classes/Shared/UAPushSettingsTokenViewController.h
@@ -26,15 +26,12 @@
 #import <UIKit/UIKit.h>
 #import <MessageUI/MessageUI.h>
 
-@interface UAPushSettingsTokenViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, MFMailComposeViewControllerDelegate> {
-    NSString *text;
-    IBOutlet UIButton *copyButton;
-    IBOutlet UIButton *emailButton;
-    IBOutlet UILabel *tokenLabel;
-}
+@interface UAPushSettingsTokenViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, MFMailComposeViewControllerDelegate>
 
-@property (nonatomic, retain) UILabel *tokenLabel;
-@property (nonatomic, retain) UIButton *emailButton;
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, retain) IBOutlet UIButton *copyButton;
+@property (nonatomic, retain) IBOutlet UIButton *emailButton;
+@property (nonatomic, retain) IBOutlet UILabel *tokenLabel;
 
 - (IBAction)copyDeviceToken;
 - (IBAction)emailDeviceToken;
