4daba28dad70dd14b1050bd1a0566785a93a37b9
marc <marc@urbanairship.com>
2013-08-20 16:52:55 -0700

    pass message in UAInboxMessageCallbackBlock

--- Airship/Inbox/UAInboxMessage.h
+++ Airship/Inbox/UAInboxMessage.h
@@ -27,7 +27,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "UAInboxMessageListDelegate.h"
 
-typedef void (^UAInboxMessageCallbackBlock)(void);
+typedef void (^UAInboxMessageCallbackBlock)(UAInboxMessage *message);
 
 @class UAInboxMessageList;
 
