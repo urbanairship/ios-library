fc8068a4e33f8b10f124317163a2ede843f7b1bb
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-08-23 12:52:58 -0700

    Call refresh session to update the push id

--- Airship/Common/UAirship.m
+++ Airship/Common/UAirship.m
@@ -205,6 +205,7 @@ UALogLevel uaLogLevel = UALogLevelUndefined;
     NSDictionary *remoteNotification = [notification.userInfo objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
 
     _sharedAirship.analytics.notificationUserInfo = remoteNotification;
+    [_sharedAirship.analytics refreshSessionWhenActive];
 
     //Send Startup Analytics Info
     //init first event
