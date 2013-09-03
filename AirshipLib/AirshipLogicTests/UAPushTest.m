954542de231ff749061f9566b7aa15f51eb13fbc
marc <marc@urbanairship.com>
2013-09-03 11:22:29 -0700

    test comments

--- AirshipLib/AirshipLogicTests/UAPushTest.m
+++ AirshipLib/AirshipLogicTests/UAPushTest.m
@@ -20,9 +20,13 @@
     self.mockRegistrationDelegate = [OCMockObject mockForProtocol:@protocol(UARegistrationDelegate)];
     self.mockRegistrationObserver = [OCMockObject mockForProtocol:@protocol(UARegistrationObserver)];
 
+    //inject the mock device API client
     [UAPush shared].deviceAPIClient = self.mockDeviceAPIClient;
+    //add our mock registration delegate
     [UAPush shared].registrationDelegate = self.mockRegistrationDelegate;
 
+    //remove all existing observers before adding our mock registration observer,
+    //so we don't end up with angry zombie mocks between cases
     [[UAPush shared] removeObservers];
     [[UAPush shared] addObserver:self.mockRegistrationObserver];
 }
@@ -34,16 +38,18 @@
 
 - (void)testUpdateRegistrationPushEnabledSuccess {
 
-    [[self.mockRegistrationDelegate expect] registerDeviceTokenSucceeded];
-
-    [[self.mockRegistrationObserver expect] registerDeviceTokenSucceeded];
-
+    //the device api client should receive a registration call.
+    //in this case, we'll call the success block immediately.
     [[[self.mockDeviceAPIClient expect] andDo:^(NSInvocation *invocation){
         void *arg;
         [invocation getArgument:&arg atIndex:3];
         UADeviceAPIClientSuccessBlock successBlock = (__bridge UADeviceAPIClientSuccessBlock) arg;
         successBlock();
     }] registerWithData:[OCMArg any] onSuccess:[OCMArg any] onFailure:[OCMArg any] forcefully:NO];
+
+    //we should get success callbacks on both the delegate and observer
+    [[self.mockRegistrationDelegate expect] registerDeviceTokenSucceeded];
+    [[self.mockRegistrationObserver expect] registerDeviceTokenSucceeded];
 
     //enable push without calling custom setter
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:UAPushEnabledSettingsKey];
@@ -53,15 +59,19 @@
 
 - (void)testUpdateRegistrationPushEnabledFailure {
 
-    [[self.mockRegistrationDelegate expect] registerDeviceTokenFailed:[OCMArg any]];
-    [[self.mockRegistrationObserver expect] registerDeviceTokenFailed:[OCMArg any]];
-
+    //the device api client should receive an registration call.
+    //in this case, we'll call the failure block immediately.
     [[[self.mockDeviceAPIClient expect] andDo:^(NSInvocation *invocation){
         void *arg;
         [invocation getArgument:&arg atIndex:4];
         UADeviceAPIClientFailureBlock failureBlock = (__bridge UADeviceAPIClientFailureBlock) arg;
+        //passing nil here instead of the usual UAHTTPRequest argument for convenience
         failureBlock(nil);
     }] registerWithData:[OCMArg any] onSuccess:[OCMArg any] onFailure:[OCMArg any] forcefully:NO];
+
+    //we should get failure callbacks on both the delegate and observer
+    [[self.mockRegistrationDelegate expect] registerDeviceTokenFailed:[OCMArg any]];
+    [[self.mockRegistrationObserver expect] registerDeviceTokenFailed:[OCMArg any]];
 
     //enable push without calling custom setter
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:UAPushEnabledSettingsKey];
@@ -69,6 +79,9 @@
 }
 
 - (void)testUpdateRegistrationPushDisabledSuccess {
+
+    //the device api client should receive an unregistration call.
+    //in this case, we'll call the success block immediately.
     [[[self.mockDeviceAPIClient expect] andDo:^(NSInvocation *invocation){
         void *arg;
         [invocation getArgument:&arg atIndex:3];
@@ -76,6 +89,7 @@
         successBlock();
     }] unregisterWithData:[OCMArg any] onSuccess:[OCMArg any] onFailure:[OCMArg any] forcefully:NO];
 
+    //we should get success callbacks on both the delegate and observer
     [[self.mockRegistrationDelegate expect] unregisterDeviceTokenSucceeded];
     [[self.mockRegistrationObserver expect] unregisterDeviceTokenSucceeded];
 
@@ -85,13 +99,18 @@
 }
 
 - (void)testUpdateRegistrationPushDisabledFailure {
+
+    //the device api client should receive an unregistration call.
+    //in this case, we'll call the failure block immediately.
     [[[self.mockDeviceAPIClient expect] andDo:^(NSInvocation *invocation){
         void *arg;
         [invocation getArgument:&arg atIndex:4];
         UADeviceAPIClientFailureBlock failureBlock = (__bridge UADeviceAPIClientFailureBlock) arg;
+        //passing nil here instead of the usual UAHTTPRequest argument for convenience
         failureBlock(nil);
     }] unregisterWithData:[OCMArg any] onSuccess:[OCMArg any] onFailure:[OCMArg any] forcefully:NO];
 
+    //we should get failure callbacks on both the delegate and observer
     [[self.mockRegistrationDelegate expect] unregisterDeviceTokenFailed:[OCMArg any]];
     [[self.mockRegistrationObserver expect] unregisterDeviceTokenFailed:[OCMArg any]];
 
