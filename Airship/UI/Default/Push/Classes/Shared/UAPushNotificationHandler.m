77a451a5419da046393f1ed1aba50899a9339fc7
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-09-11 11:21:39 -0700

    Add completion handler version of the push delegates

--- Airship/UI/Default/Push/Classes/Shared/UAPushNotificationHandler.m
+++ Airship/UI/Default/Push/Classes/Shared/UAPushNotificationHandler.m
@@ -102,14 +102,30 @@
     UA_LDEBUG(@"Received a notification while the app was already in the foreground");
 	
 	// Do something with your customData JSON, then entire notification is also available
-	
 }
 
 - (void)launchedFromNotification:(NSDictionary *)notification {
     UA_LDEBUG(@"The application was launched or resumed from a notification");
 	
 	// Do something when launched via a notification
-	
+}
+
+- (void)receivedForegroundNotification:(NSDictionary *)notification fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler{
+    UA_LDEBUG(@"Received a notification while the app was already in the foreground");
+
+	// Do something with your customData JSON, then entire notification is also available
+
+    // Call the completion handler
+    completionHandler(UIBackgroundFetchResultNoData);
+}
+
+- (void)launchedFromNotification:(NSDictionary *)notification fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    UA_LDEBUG(@"The application was launched or resumed from a notification");
+
+	// Do something when launched via a notification
+
+    // Call the completion handler
+    completionHandler(UIBackgroundFetchResultNoData);
 }
 
 @end
