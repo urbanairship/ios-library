ee97bfa03bfeeb9fbcdc5869b9d27f8551a8040a
hcrowell <helen.crowell@urbanairship.com>
2013-09-19 15:58:07 -0700

    Added PushSampleKIFTests using KIF version 2.0

---
+++ PushSample/PushSampleKIFTests/KIFUITestActor+UAAdditions.m
@@ -0,0 +1,68 @@
+/*
+ Copyright 2009-2013 Urban Airship Inc. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binaryform must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided withthe distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "KIFUITestActor+UAAdditions.h"
+#import "UATestPushDelegate.h"
+#import "UAUtils.h"
+
+#define kPushWait 10.0 // TODO: Testing only. Revert back original value 90.0
+
+@implementation KIFUITestActor (UAAdditions)
+
+// This method stores the uniqueID for the expected push notification
+- (void)setUniqueID:(NSString *)alertID {
+    return [self runBlock:^KIFTestStepResult(NSError *__autoreleasing *error) {
+        NSLog(@"Set uniqueID.");
+        UATestPushDelegate *pushDelegate = (UATestPushDelegate*) [UAPush shared].pushNotificationDelegate;
+        pushDelegate.uniqueID = alertID;
+        return KIFTestStepResultSuccess;
+    }];
+}
+
+// This method sends the push notification with the uniqueID and verifies the expected push arrived
+- (void)sendAndWaitForNotification:(NSString *)description sendPushBlock:(SendPushBlock)sendPushBlock {
+    NSString *alertID = [UAUtils UUID];
+    [self setUniqueID:alertID];
+
+    return [self runBlock:^KIFTestStepResult(NSError **error) {
+        NSLog(@"%@", description);
+        sendPushBlock(alertID);
+        [self waitForTimeInterval:kPushWait];
+        [self waitForTappableViewWithAccessibilityLabel:alertID traits:UIAccessibilityTraitButton];
+        [self tapViewWithAccessibilityLabel:alertID traits:UIAccessibilityTraitButton];
+        return KIFTestStepResultSuccess;
+    }];
+}
+
+// This method verifies the pushEnabled value
+- (void)verifyPushEnabled:(Boolean)enabled {
+    return [self runBlock:^KIFTestStepResult(NSError **error) {
+        NSLog(@"Verify PushEnabled.");
+        KIFTestCondition(([UAPush shared].pushEnabled == enabled), error, @"PushEnabled does not match expected value.");
+        return KIFTestStepResultSuccess;
+    }];
+}
+
+@end
