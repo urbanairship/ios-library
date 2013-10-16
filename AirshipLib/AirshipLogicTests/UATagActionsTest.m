8562a208ec9c7665375641a2eb07b6e074e78b1d
marc <marc@urbanairship.com>
2013-10-16 13:03:32 -0700

    UATagActionsTests

--- AirshipLib/AirshipLogicTests/UATagActionsTest.m
+++ AirshipLib/AirshipLogicTests/UATagActionsTest.m
@@ -3,102 +3,151 @@
 #import <OCMock/OCMock.h>
 
 #import "UAAction+Internal.h"
-#import "UAAddTagAction.h"
-#import "UARemoveTagAction.h"
+#import "UAAddTagsAction.h"
+#import "UARemoveTagsAction.h"
 #import "UASetTagsAction.h"
 #import "UAPush+Internal.h"
+#import "UAPush+Test.h"
 
 @interface UATagActionsTest : XCTestCase
-@property(nonatomic, strong) id mockedDeviceAPIClient;
+@property(nonatomic, strong) id mockPush;
+@property(nonatomic, strong) UAActionArguments *stringArgs;
+@property(nonatomic, strong) UAActionArguments *arrayArgs;
+@property(nonatomic, strong) UAActionArguments *emptyArrayArgs;
+@property(nonatomic, strong) UAActionArguments *badArrayArgs;
+@property(nonatomic, strong) UAActionArguments *numberArgs;
 @end
 
 @implementation UATagActionsTest
 
 - (void)setUp {
     [super setUp];
-    self.mockedDeviceAPIClient = [OCMockObject partialMockForObject:[UAPush shared].deviceAPIClient];
+    self.mockPush = [OCMockObject niceMockForClass:[UAPush class]];
+    self.stringArgs = [UAActionArguments argumentsWithValue:@"hi" withSituation:nil];
+    self.arrayArgs = [UAActionArguments argumentsWithValue:@[@"hi", @"there"] withSituation:nil];
+    self.emptyArrayArgs = [UAActionArguments argumentsWithValue:@[] withSituation:nil];
+    self.badArrayArgs = [UAActionArguments argumentsWithValue:@[@"hi", @10] withSituation:nil];
+    self.numberArgs = [UAActionArguments argumentsWithValue:@10 withSituation:nil];
+    [UAPush configure:self.mockPush];
 }
 
-- (void)validateSituationForTagAction:(UAAction *)action withArgs:(id)value {
-    UAActionArguments *args = [UAActionArguments argumentsWithValue:value withSituation:nil];
+- (void)tearDown {
+    [UAPush reset];
+    [super tearDown];
+}
 
-    XCTAssertTrue([action acceptsArguments:args], @"no situation should be acceptable");
+/**
+ * Makes sure that the passed action rejects the background situation
+ */
+- (void)validateSituationForTagAction:(UAAction *)action {
+
+    UAActionArguments *args = [UAActionArguments argumentsWithValue:@[@"hey!"] withSituation:nil];
+
+    XCTAssertTrue([action acceptsArguments:args], @"nil situation should be acceptable");
 
     for (NSString *situation in @[UASituationLaunchedFromPush, UASituationForegroundPush, UASituationLaunchedFromSpringBoard, UASituationRichPushAction]) {
         args.situation = situation;
+        NSLog(@"situation!: %@", args.situation);
         XCTAssertTrue([action acceptsArguments:args], @"any non-background situation should be valid");
     }
 
     args.situation = UASituationBackgroundPush;
     XCTAssertFalse([action acceptsArguments:args], @"background situation should be invalid");
+
+    args.situation = nil;
 }
 
-- (void)validateArgumentsForSingleTagAction:(UAAction *)action {
-    UAActionArguments *args = [UAActionArguments argumentsWithValue:@"hey!" withSituation:nil];
-    [self validateSituationForTagAction:action withArgs:args];
+/**
+ * Add/Remove tags should accept strings, empty arrays, and arrays of strings
+ */
+- (void)validateArgumentsForAddRemoveTagsAction:(UAAction *)action {
+    UAActionArguments *args = [UAActionArguments argumentsWithValue:@[@"hey!"] withSituation:nil];
+    [self validateSituationForTagAction:action];
 
-    args.value = [NSNumber numberWithInt:10];
-    XCTAssertFalse([action acceptsArguments:args], @"only strings are allowed as values");
-
-    args.value = [NSArray array];
-    XCTAssertFalse([action acceptsArguments:args], @"only strings are allowed as values");
+    XCTAssertTrue([action acceptsArguments:self.stringArgs], @"strings should be accepted");
+    XCTAssertTrue([action acceptsArguments:self.arrayArgs], @"arrays should be accepted");
+    XCTAssertTrue([action acceptsArguments:self.emptyArrayArgs], @"empty arrays should be accepted");
+    XCTAssertFalse([action acceptsArguments:self.badArrayArgs], @"arrays should only contain strings");
+    XCTAssertFalse([action acceptsArguments:self.numberArgs], @"non arrays/strings should be rejected");
 }
 
+/**
+ * Set tags should accept empty arrays, and arrays of strings
+ */
 - (void)validateArgumentsForSetTagsAction:(UAAction *)action {
     UAActionArguments *args = [UAActionArguments argumentsWithValue:@[@"foo", @"bar", @"baz"] withSituation:nil];
-    [self validateSituationForTagAction:action withArgs:args];
+    [self validateSituationForTagAction:action];
 
-    args.value = [NSNumber numberWithInt:10];
-    XCTAssertFalse([action acceptsArguments:args], @"only arrays are allowed as values");
-
-    args.value = @"howdy";
-    XCTAssertFalse([action acceptsArguments:args], @"only arrays are allowed as values");
-
-    args.value = @[@"foo", [NSNumber numberWithInt:10]];
-    XCTAssertFalse([action acceptsArguments:args], @"arrays must only contain strings");
-
-    XCTAssertTrue([action acceptsArguments:args], @"empty arrays are acceptable");
-    args.value = @[];
+    XCTAssertTrue([action acceptsArguments:self.arrayArgs], @"arrays should be accepted");
+    XCTAssertTrue([action acceptsArguments:self.emptyArrayArgs], @"empty arrays should be accepted");
+    XCTAssertFalse([action acceptsArguments:self.badArrayArgs], @"arrays should only contain strings");
+    XCTAssertFalse([action acceptsArguments:self.stringArgs], @"strings should be rejected");
+    XCTAssertFalse([action acceptsArguments:self.numberArgs], @"non arrays should be rejected");
 }
 
-- (void)testAddTagAction {
-    UAAddTagAction *action = [[UAAddTagAction alloc] init];
-    [self validateArgumentsForSingleTagAction:action];
+/**
+ * Checks argument validation and UAPush side effects of the add tags action
+ */
+- (void)testAddTagsAction {
+    UAAddTagsAction *action = [[UAAddTagsAction alloc] init];
+    [self validateArgumentsForAddRemoveTagsAction:action];
 
-    [action runWithArguments:
-     [UAActionArguments argumentsWithValue:@"hi" withSituation:nil]
+    [[self.mockPush expect] addTagToCurrentDevice:[OCMArg any]];
+    [[self.mockPush expect] updateRegistration];
+
+    [action runWithArguments:[UAActionArguments argumentsWithValue:self.stringArgs withSituation:nil]
        withCompletionHandler:^(UAActionResult *result){
-           NSSet *set = [NSSet setWithArray:[UAPush shared].tags];
-           XCTAssertTrue([set isEqualToSet:[NSSet setWithArray:@[@"hi"]]], @"tags should contain 'hi'");
-       }];
+           [self.mockPush verify];
+    }];
 
-    [action runWithArguments:
-     [UAActionArguments argumentsWithValue:@"there" withSituation:nil]
+    [[self.mockPush expect] addTagsToCurrentDevice:[OCMArg any]];
+    [[self.mockPush expect] updateRegistration];
+
+    [action runWithArguments:[UAActionArguments argumentsWithValue:self.arrayArgs withSituation:nil]
        withCompletionHandler:^(UAActionResult *result){
-           NSSet *set = [NSSet setWithArray:[UAPush shared].tags];
-           XCTAssertTrue([set isEqualToSet:[NSSet setWithArray:@[@"hi", @"there"]]], @"tags should contain 'hi', 'there'");
-       }];
-
-    //test run
+           [self.mockPush verify];
+    }];
 }
 
-- (void)testRemoveTagAction {
-    UARemoveTagAction *action = [[UARemoveTagAction alloc] init];
-    [self validateArgumentsForSingleTagAction:action];
+/**
+ * Checks argument validation and UAPush side effects of the remove tags action
+ */
+- (void)testRemoveTagsAction {
+    UARemoveTagsAction *action = [[UARemoveTagsAction alloc] init];
+    [self validateArgumentsForAddRemoveTagsAction:action];
 
-    //test run
+    [[self.mockPush expect] removeTagFromCurrentDevice:[OCMArg any]];
+    [[self.mockPush expect] updateRegistration];
+
+    [action runWithArguments:[UAActionArguments argumentsWithValue:self.stringArgs withSituation:nil]
+       withCompletionHandler:^(UAActionResult *result){
+           [self.mockPush verify];
+    }];
+
+    [[self.mockPush expect] removeTagsFromCurrentDevice:[OCMArg any]];
+    [[self.mockPush expect] updateRegistration];
+
+    [action runWithArguments:[UAActionArguments argumentsWithValue:self.arrayArgs withSituation:nil]
+       withCompletionHandler:^(UAActionResult *result){
+           [self.mockPush verify];
+    }];
+
 }
 
+/**
+ * Checks argument validation and UAPush side effects of the set tags action
+ */
 - (void)testSetTagsAction {
     UASetTagsAction *action = [[UASetTagsAction alloc] init];
     [self validateArgumentsForSetTagsAction:action];
 
-    //test run
-}
+    [[self.mockPush expect] setTags:[OCMArg any]];
+    [[self.mockPush expect] updateRegistration];
 
-- (void)tearDown {
-    [UAPush shared].tags = nil;
-    [super tearDown];
+    [action runWithArguments:[UAActionArguments argumentsWithValue:@[@"hi"] withSituation:nil]
+       withCompletionHandler:^(UAActionResult *result){
+           [self.mockPush verify];
+    }];
 }
 
 @end
