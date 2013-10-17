4859b4dc859d11f0f6b78cdddbab9820cca1735d
jtowle <jeff@urbanairship.com>
2013-10-17 09:19:37 -0700

    Bespoke cleanup.

--- InboxSample/Classes/Shared/UABespokeCloseView.h
+++ InboxSample/Classes/Shared/UABespokeCloseView.h
@@ -25,6 +25,10 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * A circle with an X in it, drawn to fill the frame.
+ * TODO: expose the margin inset and line width
+ */
 @interface UABespokeCloseView : UIView
 
 @end
