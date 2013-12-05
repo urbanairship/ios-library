32d1ee64c26ede4a3d666def22c37b0002ac4895
marc <marc@urbanairship.com>
2013-12-05 12:38:38 -0800

    Airship/Common/UAJavaScriptDelegate.h

--- Airship/Common/UAJavaScriptDelegate.h
+++ Airship/Common/UAJavaScriptDelegate.h
@@ -1,6 +1,8 @@
 
 #import <Foundation/Foundation.h>
 
+@class UAWebViewCallbackData;
+
 /**
  * A completion handler used to pass the result of a UAJavaScriptDelegate callback.
  * The value passed may be nil.
@@ -18,30 +20,35 @@ typedef void (^UAJavaScriptDelegateCompletionHandler)(NSString *script);
 @required
 
 /**
- * Delegates must implement this method. Implementations take an array of string arguments
- * and a dictionary of key-value pairs (all strings), process them, and pass a string
+ * Delegates must implement this method. Implementations take a model object representing
+ * callback data, which includes the callback name, an array of string arguments,
+ * and a dictionary of key-value pairs (all strings). After processing them, they pass a string
  * containing Javascript that will be evaluated in a message's UIWebView.
  *
- * To pass information to the delegate from a message, insert links with a "ua" scheme,
+ * If the passed callback name is not one the delegate responds to, or if no JavaScript side effect
+ * is desired, it implementations should pass nil.
+ *
+ * To pass information to the delegate from a webview, insert links with a "ua" scheme,
  * args in the path and key-value option pairs in the query string. The host
- * portion of the URL is ignored.
+ * portion of the URL is treated as the callback name.
  *
  * The basic URL format:
- * ua://callback/<args>?<key/value options>
+ * ua://callback-name/<args>?<key/value options>
  *
- * For example, to pass in three args (arg1, arg2 and arg3) and three key-value
- * options {option1:one, option2:two, option3:three}:
- * ua://callback/arg1/arg2/arg3?option1=one&amp;option2=two&amp;option3=three
+ * For example, to invoke a callback named "foo", and pass in three args (arg1, arg2 and arg3) 
+ * and three key-value options {option1:one, option2:two, option3:three}:
  *
- * The default, internal implementation is UAActionJSDelegate.
+ * ua://foo/arg1/arg2/arg3?option1=one&amp;option2=two&amp;option3=three
  *
- * @param args An array of argument values
- * @param options A dictionary of key/value query parameters
+ * The default, internal implementation of this protocol is UAActionJSDelegate.
+ * UAActionJSDelegate reserves callback names associated with running Actions, and
+ * handles those callbacks exclusively.
+ *
+ * @param data An instance of `UAWebViewCallbackData`
  * @param completionHandler A completion handler to be called with the resulting
  * string to be executed back in the JS environment.
  */
-- (void)callbackArguments:(NSArray *)args
-              withOptions:(NSDictionary *)options
-    withCompletionHandler:(UAJavaScriptDelegateCompletionHandler)completionHandler;
+- (void)callbackWithData:(UAWebViewCallbackData *)data
+   withCompletionHandler:(UAJavaScriptDelegateCompletionHandler)completionHandler;
 
 @end
