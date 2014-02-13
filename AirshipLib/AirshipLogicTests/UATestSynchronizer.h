232239435f958ff0d7cbfb84e033a89da0bd0070
marc <marc@urbanairship.com>
2014-02-12 18:19:56 -0800

    UATestSynchronizer

--- AirshipLib/AirshipLogicTests/UATestSynchronizer.h
+++ AirshipLib/AirshipLogicTests/UATestSynchronizer.h
@@ -1,13 +1,40 @@
-//
-//  UATestSynchronizer.h
-//  AirshipLib
-//
-//  Created by Marc Sciglimpaglia on 2/12/14.
-//
-//
 
 #import <Foundation/Foundation.h>
 
+/**
+ * Convenience interface for synchronizing tests across runloop iterations with dispatch semaphores.
+ */
 @interface UATestSynchronizer : NSObject
+
+#if OS_OBJECT_USE_OBJC
+/**
+ * The dispatch semaphore used for synchronization
+ */
+@property(nonatomic, strong) dispatch_semaphore_t semaphore;    // GCD objects use ARC
+#else
+@property(nonatomic, assign) dispatch_semaphore_t semaphore;    // GCD object don't use ARC
+#endif
+
+/**
+ * How long the runloop should spin for each iteration while waiting.
+ * Default is 0.1 seconds.
+ */
+@property(nonatomic, assign) NSTimeInterval runLoopInterval;
+/**
+ * How long to wait for a completion signal before timing out.
+ * Default is 2 seconds.
+ */
+@property(nonatomic, assign) NSTimeInterval timeoutInterval;
+
+/**
+ * Spin the run loop iteratively until either a completion signal is delivered,
+ * or the timeout is reached.
+ */
+- (void)wait;
+
+/**
+ * Delivers a completion signal on the semaphore.
+ */
+- (void)continue;
 
 @end
--- AirshipLib/AirshipLogicTests/UATestSynchronizer.m
+++ AirshipLib/AirshipLogicTests/UATestSynchronizer.m
@@ -1,13 +1,36 @@
-//
-//  UATestSynchronizer.m
-//  AirshipLib
-//
-//  Created by Marc Sciglimpaglia on 2/12/14.
-//
-//
 
 #import "UATestSynchronizer.h"
 
 @implementation UATestSynchronizer
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.semaphore = dispatch_semaphore_create(0);
+        //default to 0.1 seconds per run loop spin
+        self.runLoopInterval = 0.1;
+        //default to a 2 second timeout
+        self.timeoutInterval = 2;
+    }
+    return self;
+}
+
+- (void)wait {
+    NSDate *timeoutDate = [NSDate dateWithTimeIntervalSinceNow:self.timeoutInterval];
+    while (dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_NOW)  && [timeoutDate timeIntervalSinceNow] > 0) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:self.runLoopInterval]];
+    }
+}
+
+- (void)continue {
+    dispatch_semaphore_signal(self.semaphore);
+}
+
+- (void)dealloc {
+#if !OS_OBJECT_USE_OBJC
+    dispatch_release(self.semaphore);
+#endif
+}
 
 @end
