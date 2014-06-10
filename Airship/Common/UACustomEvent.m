467931bd9b031afbf76e8915f9f0226dd5986bea
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2014-06-10 16:52:10 -0700

    Add custom events

---
+++ Airship/Common/UACustomEvent.m
@@ -0,0 +1,148 @@
+/*
+ Copyright 2009-2014 Urban Airship Inc. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binaryform must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided withthe distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "UACustomEvent.h"
+#import "UAAnalytics.h"
+#import "UAirship.h"
+
+@implementation UACustomEvent
+
+#define kUACustomEventCharacterLimit 255
+#define kUACustomEventSize 1000
+
+- (NSString *)getType {
+    return @"custom_event";
+}
+
+- (instancetype)initWithName:(NSString *)eventName withValue:(NSDecimalNumber *)eventValue {
+    self = [super init];
+    if (self) {
+        self.eventName = eventName;
+        self.eventValue = eventValue;
+    }
+
+    return self;
+}
+
++ (instancetype)eventWithName:(NSString *)eventName {
+    return [[UACustomEvent alloc] initWithName:eventName withValue:nil];
+}
+
++ (instancetype)eventWithName:(NSString *)eventName valueFromString:(NSString *)eventValue {
+    NSDecimalNumber *decimalValue = [NSDecimalNumber decimalNumberWithString:eventValue];
+    return [[UACustomEvent alloc] initWithName:eventName withValue:decimalValue];
+}
+
++ (instancetype)eventWithName:(NSString *)eventName value:(NSDecimalNumber *)eventValue {
+    return [[UACustomEvent alloc] initWithName:eventName withValue:eventValue];
+}
+
+- (void)gatherIndividualData:(NSDictionary *)context {
+    // Event name
+    [self addDataWithValue:self.eventName forKey:@"event_name"];
+
+    // Attribution
+    if (!self.attributionType && self.attributionID) {
+        NSString *conversionID = [[UAirship shared].analytics.session objectForKey:@"launched_from_push_id"];
+        if (conversionID) {
+            [self addDataWithValue:@"ua_hard_open" forKey:@"attribution_type"];
+            [self addDataWithValue:conversionID forKey:@"attribution_id"];
+        }
+    } else {
+        [self addDataWithValue:self.attributionID forKey:@"attribution_id"];
+        [self addDataWithValue:self.attributionType forKey:@"attribution_type"];
+    }
+
+    // Transaction ID
+    [self addDataWithValue:self.transactionID forKey:@"transaction_id"];
+
+    // Event value
+    if (self.eventValue) {
+
+        // Move the decimal position over 6 positions
+        NSDecimalNumber *number = [self.eventValue decimalNumberByMultiplyingByPowerOf10:6];
+
+        /*
+         We use long long value here because on a 32 bit machine int and long
+         have the same range. We clamp eventValue to [-2^31, 2^31-1] so we know
+         for sure that the value multiplied by 10^6 (~2^20) will have an approximate
+         range of [-2^51, 2^51 -1] so it will always fit into the range warp9
+         is expecting [-2^63, 2^63-1].
+         */
+        [self addDataWithValue:@([number longLongValue]) forKey:@"event_value"];
+    }
+}
+
+- (NSUInteger)getEstimatedSize {
+    return 1000;
+}
+
+- (void)setEventName:(NSString *)eventName {
+    if (eventName.length > kUACustomEventCharacterLimit) {
+        UA_LERR(@"Event name is larger than %d characters.", kUACustomEventCharacterLimit);
+    } else {
+        _eventName = [eventName copy];
+    }
+}
+
+- (void)setAttributionID:(NSString *)attributionID {
+    if (attributionID.length > kUACustomEventCharacterLimit) {
+        UA_LERR(@"Event attribution ID is larger than %d characters.", kUACustomEventCharacterLimit);
+    } else {
+        _attributionID = [attributionID copy];
+    }
+}
+
+- (void)setAttributionType:(NSString *)attributionType {
+    if (attributionType.length > kUACustomEventCharacterLimit) {
+        UA_LERR(@"Event attribution type is larger than %d characters.", kUACustomEventCharacterLimit);
+    } else {
+        _attributionType = [attributionType copy];
+    }
+}
+
+- (void)setTransactionID:(NSString *)transactionID {
+    if (transactionID.length > kUACustomEventCharacterLimit) {
+        UA_LERR(@"Event transaction ID is larger than %d characters.", kUACustomEventCharacterLimit);
+    } else {
+        _transactionID = [transactionID copy];
+    }
+}
+
+- (void)setEventValue:(NSDecimalNumber *)eventValue {
+    if ([eventValue compare:@(INT32_MAX)] > 0) {
+        UA_LERR(@"Event value %@ is larger than 2^31 - 1", self.eventValue);
+    } else if ([eventValue compare:@(INT32_MIN)] < 0) {
+        UA_LERR(@"Event value %@ is smaller than -2^31", self.eventValue);
+    } else {
+        if ([eventValue isKindOfClass:[NSDecimalNumber class]]) {
+            _eventValue = eventValue;
+        } else {
+            _eventValue = [NSDecimalNumber decimalNumberWithDecimal:[eventValue decimalValue]];
+        }
+    }
+}
+
+@end
---
+++ AirshipLib/AirshipLogicTests/UACustomEventTest.m
@@ -0,0 +1,223 @@
+/*
+ Copyright 2009-2014 Urban Airship Inc. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binaryform must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided withthe distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <XCTest/XCTest.h>
+#import "UACustomEvent.h"
+#import "NSJSONSerialization+UAAdditions.h"
+
+@interface UACustomEventTest : XCTestCase
+
+@end
+
+@implementation UACustomEventTest
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+/**
+ * Test creating a custom event.
+ */
+- (void)testCustomEvent {
+    NSString *eventName =  [@"" stringByPaddingToLength:255 withString:@"EVENT_NAME" startingAtIndex:0];
+    NSString *transactionID =  [@"" stringByPaddingToLength:255 withString:@"TRANSACTION_ID" startingAtIndex:0];
+    NSString *attributinoID =  [@"" stringByPaddingToLength:255 withString:@"ATTRIBUTION_ID" startingAtIndex:0];
+    NSString *attributionType =  [@"" stringByPaddingToLength:255 withString:@"ATTRIBUTION_TYPE" startingAtIndex:0];
+
+    UACustomEvent *event = [UACustomEvent eventWithName:eventName value:@(INT32_MIN)];
+    event.transactionID = transactionID;
+    event.attributionID = attributinoID;
+    event.attributionType = attributionType;
+    [event gatherData:@{}];
+
+    XCTAssertEqualObjects(eventName, [event.data objectForKey:@"event_name"], @"Unexepcted event name.");
+    XCTAssertEqualObjects(transactionID, [event.data objectForKey:@"transaction_id"], @"Unexepcted transaction id.");
+    XCTAssertEqualObjects(attributinoID, [event.data objectForKey:@"attribution_id"], @"Unexepcted attribution id.");
+    XCTAssertEqualObjects(attributionType, [event.data objectForKey:@"attribution_type"], @"Unexepcted attribution type.");
+    XCTAssertEqualObjects(@(INT32_MIN * 1000000.0), [event.data objectForKey:@"event_value"], @"Unexepcted event value.");
+}
+
+/**
+ * Test setting an event name.
+ */
+- (void)testSetCustomEventName {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name"];
+    XCTAssertEqualObjects(event.eventName, @"event name", "255 character event name should be valid");
+
+    event.eventName = nil;
+    XCTAssertNil(event.eventName, @"Event names should be able to be cleared");
+
+
+    event.eventName =  [@"" stringByPaddingToLength:256 withString:@"EVENT_NAME" startingAtIndex:0];
+    XCTAssertNil(event.eventName, @"Event names larger than 255 characters should be ignored");
+
+    NSString *eventName = [@"" stringByPaddingToLength:255 withString:@"EVENT_NAME" startingAtIndex:0];
+    event.eventName =  eventName;
+    XCTAssertEqualObjects(event.eventName, eventName, "255 character event name should be valid");
+}
+
+/**
+ * Test setting the attribution ID.
+ */
+- (void)testSetAttributionID {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name"];
+    XCTAssertNil(event.attributionID, @"Attribution ID should default to nil");
+
+    NSString *attributionID = [@"" stringByPaddingToLength:255 withString:@"ATTRIBUTION_ID" startingAtIndex:0];
+
+    event.attributionID = attributionID;
+    XCTAssertEqualObjects(attributionID, event.attributionID, "255 character attribution IDs should be valid");
+
+    event.attributionID = nil;
+    XCTAssertNil(event.attributionID, @"Attribution ID should be able to be cleared");
+
+    event.attributionID = [@"" stringByPaddingToLength:256 withString:@"ATTRIBUTION_ID" startingAtIndex:0];
+    XCTAssertNil(event.attributionID, @"Attribution IDs larger than 255 characters should be ignored");
+}
+
+/**
+ * Test setting the attribution type.
+ */
+- (void)testSetAttritbutionType {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name"];
+    XCTAssertNil(event.attributionType, @"Attribution type should default to nil");
+
+    NSString *attributionType = [@"" stringByPaddingToLength:255 withString:@"ATTRIBUTION_TYPE" startingAtIndex:0];
+
+    event.attributionType = attributionType;
+    XCTAssertEqualObjects(attributionType, event.attributionType, "255 character attribution Types should be valid");
+
+    event.attributionType = nil;
+    XCTAssertNil(event.attributionType, @"Attribution type should be able to be cleared");
+
+    event.attributionType = [@"" stringByPaddingToLength:256 withString:@"ATTRIBUTION_TYPE" startingAtIndex:0];
+    XCTAssertNil(event.attributionID, @"Attribution types larger than 255 characters should be ignored");
+}
+
+/**
+ * Test setting the transaction ID
+ */
+- (void)testSetTransactionID {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name"];
+    XCTAssertNil(event.transactionID, @"Transaction ID should default to nil");
+
+    NSString *transactionID = [@"" stringByPaddingToLength:255 withString:@"TRANSACTION_ID" startingAtIndex:0];
+
+    event.transactionID = transactionID;
+    XCTAssertEqualObjects(transactionID, event.transactionID, "255 character transaction ID should be valid");
+
+    event.transactionID = nil;
+    XCTAssertNil(event.transactionID, @"Transaction ID should be able to be cleared");
+
+    event.transactionID = [@"" stringByPaddingToLength:256 withString:@"TRANSACTION_ID" startingAtIndex:0];
+    XCTAssertNil(event.attributionID, @"Transaction IDs larger than 255 characters should be ignored");
+}
+
+/**
+ * Test event value from a string.
+ */
+- (void)testSetEventValueString {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name" valueFromString:@"100.00"];
+    XCTAssertEqualObjects(@(100.00), event.eventValue, @"Event value should be set from a valid numeric string.");
+
+    // Max value
+    NSNumber *maxValue = @(INT32_MAX);
+    event = [UACustomEvent eventWithName:@"event name" valueFromString:[maxValue stringValue]];
+    XCTAssertEqualObjects(maxValue, event.eventValue, @"Event value should be set from a valid numeric string.");
+
+    // Above Max
+    NSDecimalNumber *aboveMax = [NSDecimalNumber decimalNumberWithDecimal:[maxValue decimalValue]];
+    aboveMax = [aboveMax decimalNumberByAdding:[NSDecimalNumber decimalNumberWithString:@"0.000001"]];
+    event = [UACustomEvent eventWithName:@"event name" valueFromString:[aboveMax stringValue]];
+    XCTAssertNil(event.eventValue, @"Event values that are too large should be ignored.");
+
+    // Min value
+    NSNumber *minValue = @(INT32_MIN);
+    event = [UACustomEvent eventWithName:@"event name" valueFromString:[minValue stringValue]];
+    XCTAssertEqualObjects(minValue, event.eventValue, @"Event value should be set from a valid numeric string.");
+
+    // Below min
+    NSDecimalNumber *belowMin = [NSDecimalNumber decimalNumberWithDecimal:[minValue decimalValue]];
+    belowMin = [belowMin decimalNumberBySubtracting:[NSDecimalNumber decimalNumberWithString:@"0.000001"]];
+    event = [UACustomEvent eventWithName:@"event name" valueFromString:[belowMin stringValue]];
+    XCTAssertNil(event.eventValue, @"Event values that are too small should be ignored.");
+
+    // 0
+    event = [UACustomEvent eventWithName:@"event name" valueFromString:@"0"];
+    XCTAssertEqualObjects(@(0), event.eventValue, @"Event value should be set from a valid numeric string.");
+}
+
+/**
+ * Test event value from an NSNumber.
+ */
+- (void)testSetEventValueNSNumber {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name" value:@(100)];
+    XCTAssertEqualObjects(@(100.00), event.eventValue, @"Event value should be set from a valid numeric string.");
+
+    // Max value
+    NSNumber *maxValue = @(INT32_MAX);
+    event = [UACustomEvent eventWithName:@"event name" value:maxValue];
+    XCTAssertEqualObjects(maxValue, event.eventValue, @"Event value should be set from a valid numeric string.");
+
+    // Above Max
+    NSDecimalNumber *aboveMax = [NSDecimalNumber decimalNumberWithDecimal:[maxValue decimalValue]];
+    aboveMax = [aboveMax decimalNumberByAdding:[NSDecimalNumber decimalNumberWithString:@"0.000001"]];
+    event = [UACustomEvent eventWithName:@"event name" value:aboveMax];
+    XCTAssertNil(event.eventValue, @"Event values that are too large should be ignored.");
+
+    // Min value
+    NSNumber *minValue = @(INT32_MIN);
+    event = [UACustomEvent eventWithName:@"event name" value:minValue];
+    XCTAssertEqualObjects(minValue, event.eventValue, @"Event value should be set from a valid numeric string.");
+
+    // Below min
+    NSDecimalNumber *belowMin = [NSDecimalNumber decimalNumberWithDecimal:[minValue decimalValue]];
+    belowMin = [belowMin decimalNumberBySubtracting:[NSDecimalNumber decimalNumberWithString:@"0.000001"]];
+    event = [UACustomEvent eventWithName:@"event name" value:belowMin];
+    XCTAssertNil(event.eventValue, @"Event values that are too small should be ignored.");
+
+    // 0
+    event = [UACustomEvent eventWithName:@"event name" value:@(0)];
+    XCTAssertEqualObjects(@(0), event.eventValue, @"Event value should be set from a valid numeric string.");
+}
+
+
+/**
+ * Test event value to data conversion.  The value should be a decimal multiplied by
+ * 10^6 and cast to a long.
+ */
+
+- (void)testEventValueToData {
+    UACustomEvent *event = [UACustomEvent eventWithName:@"event name" value:@(123.123456789)];
+    [event gatherData:@{}];
+    XCTAssertEqualObjects(@(123123456), [event.data objectForKey:@"event_value"], @"Unexepcted event value.");
+}
+
+@end
+
