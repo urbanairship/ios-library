80017a376302ab570a5f1eb45cd41882cd09b895
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2014-06-11 17:03:37 -0700

    Add event tests.

--- AirshipLib/AirshipLogicTests/UAEventTest.m
+++ AirshipLib/AirshipLogicTests/UAEventTest.m
@@ -24,16 +24,183 @@
  */
 
 #import <XCTest/XCTest.h>
+#import <OCMOCK/OCMock.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
+
 #import "UAPush+Internal.h"
 #import "UAUser+Internal.h"
 #import "UAEvent.h"
+#import "UAirship.h"
+#import "UAAnalytics.h"
+
 
 @interface UAEventTest : XCTestCase
+@property(nonatomic, strong) id analytics;
+@property(nonatomic, strong) id airship;
 
+@property(nonatomic, strong) NSMutableDictionary *session;
 @end
 
 @implementation UAEventTest
 
+- (void)setUp {
+    [super setUp];
+
+    self.analytics = [OCMockObject niceMockForClass:[UAAnalytics class]];
+    self.airship = [OCMockObject mockForClass:[UAirship class]];
+    [[[self.airship stub] andReturn:self.airship] shared];
+    [[[self.airship stub] andReturn:self.analytics] analytics];
+
+    self.session = [NSMutableDictionary dictionary];
+    [[[self.analytics stub] andReturn:self.session] session];
+}
+
+- (void)tearDown {
+    [self.analytics stopMocking];
+    [self.airship stopMocking];
+    [super tearDown];
+}
+
+/**
+ * Test app init event
+ */
+- (void)testAppInitEvent {
+    [UAUser defaultUser].username = @"user id";
+
+    [self.session setValue:@"connection type" forKey:@"connection_type"];
+    [self.session setValue:@"push id" forKey:@"launched_from_push_id"];
+    [self.session setValue:@"rich push id" forKey:@"launched_from_rich_push_id"];
+    [self.session setValue:@"true" forKey:@"foreground"];
+    [self.session setValue:@"time zone" forKey:@"time_zone"];
+    [self.session setValue:@"daylight savings" forKey:@"daylight_savings"];
+    [self.session setValue:@"notification types" forKey:@"notification_types"];
+    [self.session setValue:@"os version" forKey:@"os_version"];
+    [self.session setValue:@"package version" forKey:@"package_version"];
+    [self.session setValue:@"lib version" forKey:@"lib_version"];
+    [self.session setValue:@"some carrier" forKey:@"carrier"];
+
+    NSDictionary *expectedData = @{@"user_id": @"user id",
+                                   @"connection_type": @"connection type",
+                                   @"push_id": @"push id",
+                                   @"rich_push_id": @"rich push id",
+                                   @"foreground": @"true",
+                                   @"carrier": @"some carrier",
+                                   @"time_zone": @"time zone",
+                                   @"daylight_savings": @"daylight savings",
+                                   @"daylight_savings": @"daylight savings",
+                                   @"notification_types": @"notification types",
+                                   @"os_version": @"os version",
+                                   @"lib_version": @"lib version",
+                                   @"package_version": @"package version"};
+
+
+    UAEventAppInit *event = [UAEventAppInit event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"app_init", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventAppInitSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+
+}
+
+/**
+ * Test app foreground event
+ */
+- (void)testAppForegroundEvent {
+    [UAUser defaultUser].username = @"user id";
+
+    [self.session setValue:@"connection type" forKey:@"connection_type"];
+    [self.session setValue:@"push id" forKey:@"launched_from_push_id"];
+    [self.session setValue:@"rich push id" forKey:@"launched_from_rich_push_id"];
+    [self.session setValue:@"true" forKey:@"foreground"];
+    [self.session setValue:@"time zone" forKey:@"time_zone"];
+    [self.session setValue:@"daylight savings" forKey:@"daylight_savings"];
+    [self.session setValue:@"notification types" forKey:@"notification_types"];
+    [self.session setValue:@"os version" forKey:@"os_version"];
+    [self.session setValue:@"package version" forKey:@"package_version"];
+    [self.session setValue:@"lib version" forKey:@"lib_version"];
+    [self.session setValue:@"some carrier" forKey:@"carrier"];
+
+    // Same as app init but without the foreground key
+    NSDictionary *expectedData = @{@"user_id": @"user id",
+                                   @"connection_type": @"connection type",
+                                   @"push_id": @"push id",
+                                   @"rich_push_id": @"rich push id",
+                                   @"carrier": @"some carrier",
+                                   @"time_zone": @"time zone",
+                                   @"daylight_savings": @"daylight savings",
+                                   @"daylight_savings": @"daylight savings",
+                                   @"notification_types": @"notification types",
+                                   @"os_version": @"os version",
+                                   @"lib_version": @"lib version",
+                                   @"package_version": @"package version"};
+
+
+    UAEventAppForeground *event = [UAEventAppForeground event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"app_foreground", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventAppInitSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+
+}
+
+/**
+ * Test app exit event
+ */
+- (void)testAppExitEvent {
+    [self.session setValue:@"connection type" forKey:@"connection_type"];
+    [self.session setValue:@"push id" forKey:@"launched_from_push_id"];
+    [self.session setValue:@"rich push id" forKey:@"launched_from_rich_push_id"];
+
+    NSDictionary *expectedData = @{@"connection_type": @"connection type",
+                                   @"push_id": @"push id",
+                                   @"rich_push_id": @"rich push id"};
+
+    UAEventAppExit *event = [UAEventAppExit event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"app_exit", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventAppExitSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+}
+
+/**
+ * Test app background event
+ */
+- (void)UAEventAppBackground {
+    NSDictionary *expectedData = @{@"class_name": @""};
+
+    UAEventAppBackground *event = [UAEventAppBackground event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"app_background", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventAppExitSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+}
+
+/**
+ * Test app active event
+ */
+- (void)testAppActiveEvent {
+    NSDictionary *expectedData = @{@"class_name": @""};
+
+    UAEventAppActive *event = [UAEventAppActive event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"activity_started", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventAppActiveSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+}
+
+/**
+ * Test app inactive event
+ */
+- (void)testAppInactiveEvent {
+    NSDictionary *expectedData = @{@"class_name": @""};
+
+    UAEventAppInactive *event = [UAEventAppInactive event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"activity_stopped", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventAppActiveSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+}
 
 /**
  * Test device registration event
@@ -43,13 +210,34 @@
     [UAPush shared].channelID = @"someChannelID";
     [UAUser defaultUser].username = @"someUserID";
 
-    NSDictionary *expectedData = @{ @"device_token":@"a12312ad",
-                                    @"channel_id":@"someChannelID",
-                                    @"user_id":@"someUserID" };
+    NSDictionary *expectedData = @{@"device_token": @"a12312ad",
+                                   @"channel_id": @"someChannelID",
+                                   @"user_id": @"someUserID"};
 
-    UAEventDeviceRegistration *deviceRegistration = [UAEventDeviceRegistration event];
-    XCTAssertEqualObjects(deviceRegistration.data, expectedData, @"Event data is unexpected.");
+    UAEventDeviceRegistration *event = [UAEventDeviceRegistration event];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"device_registration", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventDeviceRegistrationSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
 }
 
+
+/**
+ * Test push received event
+ */
+- (void)testPushReceivedEvent {
+    id notification = @{ @"_": @"push id",
+                         @"_uamid": @"rich push id" };
+
+
+    NSDictionary *expectedData = @{@"rich_push_id": @"rich push id",
+                                   @"push_id": @"push id"};
+
+    UAEventPushReceived *event = [UAEventPushReceived eventWithNotification:notification];
+    XCTAssertEqualObjects(event.data, expectedData, @"Event data is unexpected.");
+    XCTAssertEqualObjects(event.eventType, @"push_received", @"Event type is unexpected.");
+    XCTAssertEqual(event.estimatedSize, kEventPushReceivedSize, @"Event is reporting wrong estimated size.");
+    XCTAssertNotNil(event.eventId, @"Event should have an ID");
+}
 
 @end
