228c1800824d51df880d88180e89c49feb10284e
marc <marc@urbanairship.com>
2014-07-23 11:22:26 -0700

    comment on isGone method

--- Airship/Inbox/UAInboxMessageData.h
+++ Airship/Inbox/UAInboxMessageData.h
@@ -89,6 +89,10 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /**
  * Indicates whether the message has been deleted from the backing store.
+ *
+ * Note: this method is more reliable for our purposes than the inherited
+ * isInserted and isDeleted methods, which both return NO after and insertion
+ * or deletion has been commited to disk once the context is saved.
  */
 - (BOOL)isGone;
 
