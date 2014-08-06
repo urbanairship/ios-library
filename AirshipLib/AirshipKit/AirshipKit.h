c8a8ac0b183a5fb819e3aa2ff904c1d5bc71370c
marc <marc@urbanairship.com>
2014-08-06 16:00:33 -0700

    angle bracket import for airshipkit umbrella header

--- AirshipLib/AirshipKit/AirshipKit.h
+++ AirshipLib/AirshipKit/AirshipKit.h
@@ -7,6 +7,6 @@ FOUNDATION_EXPORT double AirshipKitVersionNumber;
 //! Project version string for AirshipKit.
 FOUNDATION_EXPORT const unsigned char AirshipKitVersionString[];
 
-#import "AirshipLib.h"
+#import <AirshipKit/AirshipLib.h>
 
 
