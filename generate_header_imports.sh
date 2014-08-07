f7696a3cb558b1cb72b3d0862730183b5a21ac49
marc <marc@urbanairship.com>
2014-08-07 13:20:39 -0700

    docs

--- generate_header_imports.sh
+++ generate_header_imports.sh
@@ -2,4 +2,6 @@
 
 cd Airship
 
+#find all header files, excluding UASQLite, the internal headers, and AirshipLib.h,
+#and convert to Obj-C import statements
 find . -type f -name '*.h' ! -name UASQLite.h  ! -name '*+Internal.h' ! -name 'AirshipLib.h' ! -path './UI/*' -exec basename {} \; | awk '{print "#import \"" $0"\""}' > AirshipLib.h
