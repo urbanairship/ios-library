1f4306bf232c4e28a69616ca36a1add723aee71b
jtowle <jeff@urbanairship.com>
2014-08-07 09:29:19 -0700

    [MB-486] Updated CI script for iOS8, updated gitconfig and fixed a project file thing in the process.

--- run-ci-tasks.sh
+++ run-ci-tasks.sh
@@ -4,7 +4,8 @@ set -o pipefail
 # Available Xcode Apps (Aug 6 2014)
 # XCODE_5_0_2_APP
 # XCODE_5_1_1_APP
-# XCODE_6_BETA_5_APP
+# XCODE_6_BETA_4_APP
+# XCODE_6_BETA_5_APP (doesn't appear to work for headless tests - times out)
 
 # Additional versions should be set up on the build machine, and your own for testing
 # Your ~/.bash_profile might look soemthing like:
@@ -12,7 +13,7 @@ set -o pipefail
 # export XCODE_5_1_1_APP=/Applications/Xcode-5.1.1.app
 # export XCODE_6_BETA_5_APP=/Applications/Xcode6-Beta5.app
 
-XCODE_APP=$XCODE_5_1_1_APP
+XCODE_APP=$XCODE_6_BETA_4_APP
 
 if [ -z "$XCODE_APP" ]; then
   echo "Looks like you're missing Xcode!"
@@ -34,7 +35,7 @@ rm -rf test-output
 mkdir -p test-output
 
 # Run our Logic Tests
-xcrun xcodebuild -destination 'platform=iOS Simulator,OS=7.0,name=iPhone Retina (4-inch)' -project AirshipLib/AirshipLib.xcodeproj -scheme AirshipLib test | tee test-output/XCTEST-LOGIC.out
+xcrun xcodebuild -destination 'platform=iOS Simulator,OS=8.0,name=iPhone 5s' -project AirshipLib/AirshipLib.xcodeproj -scheme AirshipLib test | tee test-output/XCTEST-LOGIC.out
 
 # Run our Application Tests
-xcrun xcodebuild -destination 'platform=iOS Simulator,OS=7.0,name=iPhone Retina (4-inch)' -project PushSample/PushSampleLib.xcodeproj -scheme PushSample test | tee test-output/XCTEST-APPLICATION.out
+xcrun xcodebuild -destination 'platform=iOS Simulator,OS=8.0,name=iPhone 5s' -project PushSample/PushSampleLib.xcodeproj -scheme PushSample test | tee test-output/XCTEST-APPLICATION.out
