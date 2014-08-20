e7259f3ceedbf80a0403999070a6273577be8fb5
marc <marc@urbanairship.com>
2014-08-20 16:32:11 -0700

    AirshipKitTests

---
+++ AirshipLib/AirshipKitTests/AirshipKit_Tests.m
@@ -0,0 +1,11 @@
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <AirshipKit/AirshipKit.h>
+
+@interface AirshipKit_Tests : XCTestCase
+@end
+
+@implementation AirshipKit_Tests
+@end
