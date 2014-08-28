ef930be6de15e673c36acb6fd2aa1c8174c0e41a
marc <marc@urbanairship.com>
2014-08-28 10:22:04 -0700

    exclude AirshipKit.framework contents from generate_header_imports script

--- scripts/generate_header_imports.sh
+++ scripts/generate_header_imports.sh
@@ -5,6 +5,7 @@ ROOT_PATH=`dirname "${0}"`/../
 
 cd "${ROOT_PATH}/Airship"
 
-#find all header files, excluding UASQLite, the internal headers, and AirshipLib.h,
-#and convert to Obj-C import statements
-find . -type f -name '*.h' ! -name UASQLite.h  ! -name '*+Internal.h' ! -name 'AirshipLib.h' ! -path './UI/*' -exec basename {} \; | awk '{print "#import \"" $0"\""}' > AirshipLib.h
+#find all header files, excluding UASQLite, the internal headers, AirshipLib.h,
+#UI, and AirshipKit.framework, and convert to Obj-C import statements
+
+find . -type f -name '*.h' ! -name UASQLite.h  ! -name '*+Internal.h' ! -name 'AirshipLib.h' ! -path './UI/*' ! -path './AirshipKit.framework/*' -exec basename {} \; | awk '{print "#import \"" $0"\""}' > AirshipLib.h
