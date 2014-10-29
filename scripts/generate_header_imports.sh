88ba6b701e6e32ba66639c713f2457001a99d5b5
marc <marc@urbanairship.com>
2014-10-29 16:48:42 -0700

    [MB-707] safety quotes

--- scripts/generate_header_imports.sh
+++ scripts/generate_header_imports.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
 # navigate to the public headers directory of the built target
-cd ${BUILT_PRODUCTS_DIR}/AirshipKit.framework/Headers
+cd "${BUILT_PRODUCTS_DIR}/AirshipKit.framework/Headers"
 
 # find all public headers, excluding AirshipLib/AirshipKit and convert into
 # objective-c import statements, overwriting AirshipLib.h
 find . -type f -name '*.h' ! -name 'AirshipLib.h' ! -name 'AirshipKit.h'  -exec basename {} \; | awk '{print "#import \"" $0"\""}' > AirshipLib.h 
 
 # copy new AirshipLib.h back into the top-level Airship directory
-cp AirshipLib.h ${SRCROOT}/../Airship/
+cp AirshipLib.h "${SRCROOT}/../Airship/"
