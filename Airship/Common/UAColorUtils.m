df3c98f1fcf3e503c64f49fd14c1f82bbca4cffa
marc <marc@urbanairship.com>
2015-03-03 16:00:18 -0800

    [MB-964] proper logging for UAColorUtils

--- Airship/Common/UAColorUtils.m
+++ Airship/Common/UAColorUtils.m
@@ -41,7 +41,7 @@
     CGFloat red = 0.0, green = 0.0, blue = 0.0, alpha = 0.0;
 
     if (![color getRed:&red green:&green blue:&blue alpha:&alpha]) {
-        NSLog(@"Unable to convert color %@ to RGBA colorspace", color);
+        UA_LDEBUG(@"Unable to convert color %@ to RGBA colorspace", color);
         return nil;
     };
 
