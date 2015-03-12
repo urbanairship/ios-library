2d97fb23e7efa5ccdd79792a4a7e7c7c68dab71e
hcrowell <helen.crowell@urbanairship.com>
2015-03-12 16:09:22 -0700

    [MB-985] Added and updated copyright headers with 2015.

--- Airship/Common/UAUserAPIClient.m
+++ Airship/Common/UAUserAPIClient.m
@@ -1,5 +1,5 @@
 /*
- Copyright 2009-2014 Urban Airship Inc. All rights reserved.
+ Copyright 2009-2015 Urban Airship Inc. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
@@ -7,7 +7,7 @@
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
 
- 2. Redistributions in binaryform must reproduce the above copyright notice,
+ 2. Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
 
