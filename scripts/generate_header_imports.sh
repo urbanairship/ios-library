59e8a0a9aa4f149edf086f55eb940dd882123488
marc <marc@urbanairship.com>
2015-05-29 11:58:30 -0700

    [MB-1105] exclude UI and subdirectories from framework header script

--- scripts/generate_header_imports.sh
+++ scripts/generate_header_imports.sh
@@ -10,9 +10,10 @@ FRAMEWORK_HEADERS_DIR="${BUILT_PRODUCTS_DIR}/AirshipKit.framework/Headers"
 # Navigate to the Airship directory
 cd $AIRSHIP_DIR
 
-# Find all public headers, excluding AirshipLib and internal classes, copy to the framework headers directory, and convert into
-# objective-c import statements for reference in the umbrella header
-find . -type f -name '*.h' ! -name 'AirshipLib.h' ! -name '*+Internal.h' \
+# Find all public headers, excluding AirshipLib, internal classes, and UI
+# Copy headers to the framework headers directory
+# Collect all headers as obj-c import statments into an umbrella header named AirshipLib.h
+find . -type f -name '*.h' ! -name 'AirshipLib.h' ! -name '*+Internal.h' ! -path './UI/*'\
   -exec cp {} $FRAMEWORK_HEADERS_DIR/ \; \
   -exec basename {} \; | awk '{print "#import \"" $0"\""}' > $FRAMEWORK_HEADERS_DIR/AirshipLib.h
 
