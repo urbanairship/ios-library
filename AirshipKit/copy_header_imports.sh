6371297a6056692e63875943660d6c87b404112d
marc <marc@urbanairship.com>
2015-07-14 19:29:22 -0700

    [MB-1288] rework header imports scripts

--- AirshipKit/copy_header_imports.sh
+++ AirshipKit/copy_header_imports.sh
@@ -4,16 +4,14 @@ AIRSHIP_DIR="${SRCROOT}/../Airship"
 FRAMEWORK_HEADERS_DIR="${BUILT_PRODUCTS_DIR}/AirshipKit.framework/Headers"
 
 # Navigate to the public headers directory of the framework target and remove any existing headers
-#cd $FRAMEWORK_HEADERS_DIR
-#rm -r *.h
 
-# Navigate to the Airship directory
-cd $AIRSHIP_DIR
+cd $FRAMEWORK_HEADERS_DIR
+rm -rf *.h
 
-# Find all public headers, excluding AirshipLib, internal classes, and UI
+# Find all public headers, excluding internal and UI
 # Copy headers to the framework headers directory
-# Collect all headers as obj-c import statments into an umbrella header named AirshipLib.h
+
+cd $AIRSHIP_DIR
 find . -type f -name '*.h' ! -name '*+Internal.h' ! -path './UI/*'\
   -exec cp {} $FRAMEWORK_HEADERS_DIR/ \; \
-  -exec basename {} \; | awk '{print "#import \"" $0"\""}' > $FRAMEWORK_HEADERS_DIR/AirshipLib.h
 
