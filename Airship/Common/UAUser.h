6ae3684e0fcbe336c26c3a8ea56615560e6564f5
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2015-08-03 14:38:30 -0700

    [MB-1339] Restore and deprecate user's userUpdateBackgroundTask property

--- Airship/Common/UAUser.h
+++ Airship/Common/UAUser.h
@@ -60,4 +60,11 @@ extern NSString * const UAUserCreatedNotification;
  */
 @property (nonatomic, readonly, copy) NSString *url;
 
+/**
+ * Background task identifier used to perform user updates in the background.
+ *
+ * @deprecated As of version 6.1.4. This property is no longer used.
+ */
+@property (nonatomic, assign, readonly) UIBackgroundTaskIdentifier userUpdateBackgroundTask __attribute__((deprecated("As of version 6.1.4")));
+
 @end
