b8fdd28e7d4200d6d1cfe86928ca2a6235e18eee
marc <marc@urbanairship.com>
2016-01-07 16:09:16 -0800

    [MB-1556] don't copy headers into AirshipKit unless they're empty or changed

--- AirshipKit/copy_header_imports.sh
+++ AirshipKit/copy_header_imports.sh
@@ -2,6 +2,16 @@
 
 AIRSHIP_DIR="${SRCROOT}/../Airship"
 FRAMEWORK_HEADERS_DIR="${BUILT_PRODUCTS_DIR}/AirshipKit.framework/Headers"
+SOURCE_LIB_HEADER="${AIRSHIP_DIR}/AirshipLib.h"
+TARGET_LIB_HEADER="${FRAMEWORK_HEADERS_DIR}/AirshipLib.h"
+
+# If there's already an AirshipLib.h in the framework headers directory
+if [ -a ${FRAMEWORK_HEADERS_DIR}/AirshipLib.h ]; then
+    # If the contents haven't changed, exit early
+    if diff -q "${SOURCE_LIB_HEADER}" "${TARGET_LIB_HEADER}" > /dev/null; then
+        exit 0
+    fi
+fi
 
 # Navigate to the public headers directory of the framework target and remove any existing headers
 
