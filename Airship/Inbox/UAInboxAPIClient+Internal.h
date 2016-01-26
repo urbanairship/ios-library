0b18f16c33105abe6bd6ec861d2e42d4151a6386
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-01-26 15:49:56 -0800

    [MB-1582] Update copyright header

--- Airship/Inbox/UAInboxAPIClient+Internal.h
+++ Airship/Inbox/UAInboxAPIClient+Internal.h
@@ -1,5 +1,5 @@
 /*
- Copyright 2009-2015 Urban Airship Inc. All rights reserved.
+ Copyright 2009-2016 Urban Airship Inc. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
@@ -24,7 +24,7 @@
  */
 
 /*
- Copyright 2009-2015 Urban Airship Inc. All rights reserved.
+ Copyright 2009-2016 Urban Airship Inc. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
