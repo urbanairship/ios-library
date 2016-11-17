1cf401fcb2011014ca39d6ec9c1d39ad365abf2c
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-11-17 12:10:32 -0800

    Add script to update versions

---
+++ scripts/update_version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+SCRIPT_DIRECTORY=`dirname "$0"`
+ROOT_PATH=`dirname "${0}"`/../
+
+VERSION=$1
+
+if [ -z "$1" ]
+  then
+    echo "No version number supplied"
+    exit
+fi
+
+# Pods
+sed -i '' "s/\(s.version *= *\)\".*\"/\1\"$VERSION\"/g" $ROOT_PATH/UrbanAirship-iOS-SDK.podspec
+sed -i '' "s/\(s.version *= *\)\".*\"/\1\"$VERSION\"/g" $ROOT_PATH/UrbanAirship-iOS-AppExtensions.podspec
+
+# Airship Config
+sed -i '' "s/\CURRENT_PROJECT_VERSION.*/CURRENT_PROJECT_VERSION = $VERSION/g" $ROOT_PATH/AirshipKit/AirshipConfig.xcconfig
+
+
+
