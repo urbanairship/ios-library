e42d45c4bb84fea032f65a67d7e1ebf834c923f8
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2017-05-09 13:06:07 -0700

    [MB-2219] Fix encoded arguments in js bridge

--- AirshipKit/AirshipKitTests/UAWebViewCallDataTest.m
+++ AirshipKit/AirshipKitTests/UAWebViewCallDataTest.m
@@ -20,7 +20,7 @@
     [super tearDown];
 }
 
-- (void)testcallDataForURL {
+- (void)testCallDataForURL {
     UAWebViewCallData *data = [UAWebViewCallData callDataForURL:
                                [NSURL URLWithString:@"uairship://whatever/argument-one/argument-two?foo=bar&foo=barbar&foo"] webView:self.mockWebView];
     XCTAssertNotNil(data, @"data should be non-nil");
@@ -37,6 +37,16 @@
 
     expectedValues = @[@"bar"];
     XCTAssertEqualObjects([data.options objectForKey:@"foo"], expectedValues, @"key 'foo' should have values 'bar'");
+}
+
+- (void)testCallDataForURLEncodedArguments {
+    UAWebViewCallData *data = [UAWebViewCallData callDataForURL:
+                               [NSURL URLWithString:@"uairship://run-action-cb/%5Eu/%22https%3A%2F%2Fdocs.urbanairship.com%2Fengage%2Frich-content-editor%2F%23rich-content-image%22/ua-cb-2"] webView:self.mockWebView];
+
+    XCTAssertEqual(data.arguments.count, 3);
+    XCTAssertEqualObjects([data.arguments objectAtIndex:0], @"^u");
+    XCTAssertEqualObjects([data.arguments objectAtIndex:1], @"\"https://docs.urbanairship.com/engage/rich-content-editor/#rich-content-image\"");
+    XCTAssertEqualObjects([data.arguments objectAtIndex:2], @"ua-cb-2");
 }
 
 @end
