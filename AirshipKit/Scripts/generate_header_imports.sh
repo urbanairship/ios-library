07b71a64c4551c7bce70ca718fb599d38be6e72d
Marc Sciglimpaglia <marc@urbanairship.com>
2018-01-26 16:32:31 -0800

    [MB-2496] leave unchanged AirshipLib headers alone, again

--- AirshipKit/Scripts/generate_header_imports.sh
+++ AirshipKit/Scripts/generate_header_imports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 SCRIPT_DIRECTORY=`dirname "$0"`
-ROOT_PATH=$SCRIPT_DIRECTORY/..
+ROOT_PATH="${SCRIPT_DIRECTORY}/.."
 
 if [[ $# -lt 2 ]]; then
     echo "Usage: $1 common OS-SPECIFIER"
@@ -9,20 +9,35 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
-echo "$ROOT_PATH"
-SOURCE_LIB_HEADER="$ROOT_PATH/AirshipKit/$2/AirshipLib.h"
+echo "${ROOT_PATH}"
+TMP_LIB="/tmp/AirshipKit/$2"
+TMP_LIB_HEADER="${TMP_LIB}/AirshipLib.h"
+SOURCE_LIB_HEADER="${ROOT_PATH}/AirshipKit/$2/AirshipLib.h"
 
 # Find all public headers, excluding AirshipLib and UI
 # Collect all headers as obj-c import statments into an umbrella header
-rm "$SOURCE_LIB_HEADER" 2>/dev/null || true
-touch "$SOURCE_LIB_HEADER"
+rm "${TMP_LIB_HEADER}" 2>/dev/null || true
+mkdir -p "${TMP_LIB}" && touch "${TMP_LIB_HEADER}"
 
-echo "Generated file: ${SOURCE_LIB_HEADER}"
+echo "Generated file: ${TMP_LIB_HEADER}"
 
 for headerSubfolder in "$@"
 do
-    if [[ -d "$ROOT_PATH"/AirshipKit/${headerSubfolder} ]]; then
-        find -s "$ROOT_PATH"/AirshipKit/${headerSubfolder} -type f -name '*.h' ! -name 'AirshipLib.h' ! -name 'AirshipKit.h' ! -name '*+Internal*.h' ! -path './UI/*' \
-    -exec basename {} \; | awk '{print "#import \"" $1"\""}' >> "$SOURCE_LIB_HEADER"
+    if [[ -d "${ROOT_PATH}"/AirshipKit/${headerSubfolder} ]]; then
+        find -s "${ROOT_PATH}"/AirshipKit/${headerSubfolder} -type f -name '*.h' ! -name 'AirshipLib.h' ! -name 'AirshipKit.h' ! -name '*+Internal*.h' ! -path './UI/*' \
+    -exec basename {} \; | awk '{print "#import \"" $1"\""}' >> "${TMP_LIB_HEADER}"
     fi
 done
+
+# If there's already an AirshipLib.h in the framework headers directory
+if [ -a "${SOURCE_LIB_HEADER}" ]; then
+    # If the contents haven't changed, exit early
+    if diff -q "${SOURCE_LIB_HEADER}" "${TMP_LIB_HEADER}" > /dev/null; then
+        exit 0
+    fi
+fi
+
+echo "Generated file: ${SOURCE_LIB_HEADER}"
+
+cp "${TMP_LIB_HEADER}" "${SOURCE_LIB_HEADER}"
+
