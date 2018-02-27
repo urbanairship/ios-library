10e11e2f2eb222c1554cf4e1aa0bdbf3e794a955
Brian Batchelder <BrianBatchelder@users.noreply.github.com>
2018-02-27 10:34:43 -0800

    Make `UAActionScheduleInfo scheduleInfoWithJSON:error:` public. (#1437)

--- AirshipKit/AirshipKit/common/UAActionScheduleInfo+Internal.h
+++ AirshipKit/AirshipKit/common/UAActionScheduleInfo+Internal.h
@@ -4,21 +4,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-
 @interface UAActionScheduleInfo()
-
-///---------------------------------------------------------------------------------------
-/// @name Action Schedule Info Factories
-///---------------------------------------------------------------------------------------
-
-/**
- * Factory method to create an action schedule info from a JSON payload.
- *
- * @param json The JSON payload.
- * @param error An NSError pointer for storing errors, if applicable.
- * @return An action schedule info or `nil` if the JSON is invalid.
- */
-+ (nullable instancetype)scheduleInfoWithJSON:(id)json error:(NSError * _Nullable *)error;
 
 @end
 
