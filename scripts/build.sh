78b4fe074d03217b40b35d7e5509bb5d4d8f8075
Brian Batchelder <BrianBatchelder@users.noreply.github.com>
2019-11-13 14:57:13 -0800

    [MOBILE-922] Reorganize Bintray distribution (#1810)

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,9 +11,6 @@ TEMP_DIR=$(mktemp -d /tmp/build-XXXXX)
 DESTINATION=$ROOT_PATH/build
 STAGING=$DESTINATION/staging
 STATIC_DESTINATION=$STAGING/static
-CORE_DESTINATION=$STAGING/core
-LOCATION_DESTINATION=$STAGING/location
-EXTENSIONS_DESTINATION=$STAGING/extensions
 
 VERSION=$(awk <$ROOT_PATH/AirshipKit/AirshipConfig.xcconfig "\$1 == \"CURRENT_PROJECT_VERSION\" { print \$3 }")
 
@@ -362,7 +359,7 @@ then
   # Verify bitcode is enabled in the fat binary
   otool -l "${TEMP_DIR}/AirshipLib/libUAirship-${VERSION}.a" | grep __LLVM
 
-  echo -ne "\n\n *********** BUILDING LIBUALocation *********** \n\n"
+  echo -ne "\n\n *********** BUILDING LIBUALOCATION *********** \n\n"
 
   # iphoneOS
   xcrun xcodebuild -quiet -configuration "Release" \
@@ -403,98 +400,6 @@ then
   # Verify bitcode is enabled in the fat binary
   otool -arch arm64 -l "${TEMP_DIR}/AirshipLocationLib/libUALocation-${VERSION}.a" | grep __LLVM
 
-fi
-
-######################
-# Package distribution
-######################
-
-if [ $PACKAGE = true ]
-then
-  echo -ne "\n\n *********** PACKAGING RELEASE *********** \n\n"
-
-  # Stage AirshipKit
-  echo "Staging AirshipKit"
-  cp -R "${ROOT_PATH}/AirshipKit" "${STAGING}"
-
-  # Stage AirshipLocationKit
-  echo "Staging AirshipLocationKit"
-  cp -R "${ROOT_PATH}/AirshipLocationKit" "${STAGING}"
-
-  # Stage AirshipAppExtensions
-  echo "Staging AirshipAppExtensions"
-  cp -R "${ROOT_PATH}/AirshipAppExtensions" "${STAGING}"
-
-  # Stage Sample
-  echo "Staging Sample"
-  cp -R "${ROOT_PATH}/Sample" "${STAGING}"
-
-  # Stage SwiftSample
-  echo "Staging SwiftSample"
-  cp -R "${ROOT_PATH}/SwiftSample" "${STAGING}"
-
-  # Stage Static Library
-  echo "Staging Airship"
-  mkdir -p ${STATIC_DESTINATION}/Airship/Headers
-  find ${ROOT_PATH}/AirshipKit/AirshipKit/common -type f -name '*.h' ! -name 'AirshipKit.h' ! -name '*+Internal*.h'  -exec cp {} ${STATIC_DESTINATION}/Airship/Headers \;
-  find ${ROOT_PATH}/AirshipKit/AirshipKit/ios -type f -name '*.h' ! -name 'AirshipKit.h' ! -name '*+Internal*.h'  -exec cp {} ${STATIC_DESTINATION}/Airship/Headers \;
-  find ${ROOT_PATH}/AirshipLocationKit/AirshipLocationKit -type f -name '*.h' ! -name 'AirshipLocationKit.h' ! -name '*+Internal*.h'  -exec cp {} ${STATIC_DESTINATION}/Airship/Headers \;
-  cp "${TEMP_DIR}/AirshipLib/libUAirship-${VERSION}.a" "${STATIC_DESTINATION}/Airship"
-  cp "${TEMP_DIR}/AirshipLocationLib/libUALocation-${VERSION}.a" "${STATIC_DESTINATION}/Airship"
-  cp -R "${TEMP_DIR}/AirshipResources/Release-iphoneos/AirshipResources.bundle" "${STATIC_DESTINATION}/Airship"
-
-  # Stage Core XCFramework
-  echo "Staging Core XCFramework"
-  mkdir -p "${CORE_DESTINATION}/AirshipKit.xcframework"
-  cp -a "${TEMP_DIR}/AirshipKit.xcframework/." "${CORE_DESTINATION}/AirshipKit.xcframework/"
-
-  # Stage Location XCFramework
-  echo "Staging Location XCFramework"
-  mkdir -p "${LOCATION_DESTINATION}/AirshipKitLocation.xcframework"
-  cp -a "${TEMP_DIR}/AirshipKitLocation.xcframework/." "${LOCATION_DESTINATION}/AirshipKitLocation.xcframework/"
-  
-  # Stage Extensions XCFramework
-  echo "Staging Extensions XCFramework"
-  mkdir -p "${EXTENSIONS_DESTINATION}/AirshipAppExtensions.xcframework"
-  cp -a "${TEMP_DIR}/AirshipAppExtensions.xcframework/." "${EXTENSIONS_DESTINATION}/AirshipAppExtensions.xcframework/"
-
-  # Copy LICENSE, README and CHANGELOG
-  cp "${ROOT_PATH}/CHANGELOG.md" "${STAGING}"
-  cp "${ROOT_PATH}/README.md" "${STAGING}"
-  cp "${ROOT_PATH}/LICENSE" "${STAGING}"
-
-  # Build info
-  BUILD_INFO=$STAGING/BUILD_INFO
-  echo "Airship SDK v${VERSION}" >> $BUILD_INFO
-  echo "Build time: `date`" >> $BUILD_INFO
-  echo "SDK commit: `git log -n 1 --format='%h'`" >> $BUILD_INFO
-  echo "Xcode version: `xcrun xcodebuild -version | tr '\r\n' ' '`" >> $BUILD_INFO
-
-  # Additional build info
-  if test -f $ROOT_PATH/BUILD_INFO;
-  then cat $ROOT_PATH/BUILD_INFO >> $BUILD_INFO;
-  fi
-
-  # Clean up any unwanted files
-  rm -rf `find ${STAGING} -name "*.orig" `
-  rm -rf `find ${STAGING} -name "*KIF-Info.plist" `
-  rm -rf `find ${STAGING} -name "*.mode1v3" `
-  rm -rf `find ${STAGING} -name "*.pbxuser" `
-  rm -rf `find ${STAGING} -name "*.perspective*" `
-  rm -rf `find ${STAGING} -name "xcuserdata" `
-  rm -rf `find ${STAGING} -name "AirshipConfig.plist" `
-
-  # Rename sample plists
-  mv -f ${STAGING}/Sample/AirshipConfig.plist.sample ${STAGING}/Sample/AirshipConfig.plist
-  mv -f ${STAGING}/SwiftSample/AirshipConfig.plist.sample ${STAGING}/SwiftSample/AirshipConfig.plist
-
-  # Generate the ZIP
-  cd $STAGING
-  zip -r -X libUAirship-$VERSION.zip .
-  cd -
-
-  # Move zip
-  mv $STAGING/libUAirship-$VERSION.zip $DESTINATION
 fi
 
 ############
@@ -557,4 +462,76 @@ then
 
   # Workaround the missing module version
   find $STAGING/Documentation -name '*.html' -print0 | xargs -0 sed -i "" "s/\$AIRSHIP_VERSION/${VERSION}/g"
+fi
+
+######################
+# Package distribution
+######################
+
+if [ $PACKAGE = true ]
+then
+  echo -ne "\n\n *********** PACKAGING RELEASE *********** \n\n"
+
+  # Stage Static Library
+  echo "Staging Airship"
+  mkdir -p ${STATIC_DESTINATION}/Airship/Headers
+  find ${ROOT_PATH}/AirshipKit/AirshipKit/common -type f -name '*.h' ! -name 'AirshipKit.h' ! -name '*+Internal*.h'  -exec cp {} ${STATIC_DESTINATION}/Airship/Headers \;
+  find ${ROOT_PATH}/AirshipKit/AirshipKit/ios -type f -name '*.h' ! -name 'AirshipKit.h' ! -name '*+Internal*.h'  -exec cp {} ${STATIC_DESTINATION}/Airship/Headers \;
+  find ${ROOT_PATH}/AirshipLocationKit/AirshipLocationKit -type f -name '*.h' ! -name 'AirshipLocationKit.h' ! -name '*+Internal*.h'  -exec cp {} ${STATIC_DESTINATION}/Airship/Headers \;
+  cp "${TEMP_DIR}/AirshipLib/libUAirship-${VERSION}.a" "${STATIC_DESTINATION}/Airship"
+  cp "${TEMP_DIR}/AirshipLocationLib/libUALocation-${VERSION}.a" "${STATIC_DESTINATION}/Airship"
+  cp -R "${TEMP_DIR}/AirshipResources/Release-iphoneos/AirshipResources.bundle" "${STATIC_DESTINATION}/Airship"
+  # Add a README to the static folder with a deprecation notice. 
+  echo "# Airship iOS Static Library
+    This library is now deprecated, and will be removed in Airship SDK 13. Please use Airship iOS XCFrameworks found in the root directory of this package.
+    " > ${STATIC_DESTINATION}/README.md
+
+  # Stage Core XCFramework
+  echo "Staging Core XCFramework"
+  mkdir -p "${STAGING}/AirshipKit.xcframework"
+  cp -a "${TEMP_DIR}/AirshipKit.xcframework/." "${STAGING}/AirshipKit.xcframework/"
+
+  # Stage Location XCFramework
+  echo "Staging Location XCFramework"
+  mkdir -p "${STAGING}/AirshipKitLocation.xcframework"
+  cp -a "${TEMP_DIR}/AirshipKitLocation.xcframework/." "${STAGING}/AirshipKitLocation.xcframework/"
+  
+  # Stage Extensions XCFramework
+  echo "Staging Extensions XCFramework"
+  mkdir -p "${STAGING}/AirshipAppExtensions.xcframework"
+  cp -a "${TEMP_DIR}/AirshipAppExtensions.xcframework/." "${STAGING}/AirshipAppExtensions.xcframework/"
+
+  # Copy LICENSE, README and CHANGELOG
+  cp "${ROOT_PATH}/CHANGELOG.md" "${STAGING}"
+  cp "${ROOT_PATH}/README.md" "${STAGING}"
+  cp "${ROOT_PATH}/LICENSE" "${STAGING}"
+
+  # Build info
+  BUILD_INFO=$STAGING/BUILD_INFO
+  echo "Airship SDK v${VERSION}" >> $BUILD_INFO
+  echo "Build time: `date`" >> $BUILD_INFO
+  echo "SDK commit: `git log -n 1 --format='%h'`" >> $BUILD_INFO
+  echo "Xcode version: `xcrun xcodebuild -version | tr '\r\n' ' '`" >> $BUILD_INFO
+
+  # Additional build info
+  if test -f $ROOT_PATH/BUILD_INFO;
+  then cat $ROOT_PATH/BUILD_INFO >> $BUILD_INFO;
+  fi
+
+  # Clean up any unwanted files
+  rm -rf `find ${STAGING} -name "*.orig" `
+  rm -rf `find ${STAGING} -name "*KIF-Info.plist" `
+  rm -rf `find ${STAGING} -name "*.mode1v3" `
+  rm -rf `find ${STAGING} -name "*.pbxuser" `
+  rm -rf `find ${STAGING} -name "*.perspective*" `
+  rm -rf `find ${STAGING} -name "xcuserdata" `
+  rm -rf `find ${STAGING} -name "AirshipConfig.plist" `
+
+  # Generate the ZIP
+  cd $STAGING
+  zip -r -X libUAirship-$VERSION.zip .
+  cd -
+
+  # Move zip
+  mv $STAGING/libUAirship-$VERSION.zip $DESTINATION
 fi
