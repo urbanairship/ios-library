f755ed3fd0f99a471b11845f0cfb712eb7603028
Marc Sciglimpaglia <marc@urbanairship.com>
2019-12-05 13:02:50 -0800

    [MOBILE-1098] SDK 13 migration guide

--- Airship/AirshipMessageCenter/Source/Actions/UAMessageCenterAction.h
+++ Airship/AirshipMessageCenter/Source/Actions/UAMessageCenterAction.h
@@ -8,7 +8,8 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  * Requests the inbox be displayed.
  *
- * The action will call the UAInboxDelegate `showInboxMessage:` or `showInbox` depending on if the message ID is available or not.
+ * The action will call the UAMessageCenterDisplayDelegate `displayMessageCenterForMessageID:animated:` or
+ * `displayMessageCenterAnimated:` depending on whether the message ID is available or not.
  *
  * This action is registered under the name `_uamid`.
  *
