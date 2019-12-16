617aa0bd4cfae892f8eef4700ebd7067c0f73263
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2019-12-16 08:36:11 -0800

    [MOBILE-1050] Clean up deploy (#1867)
    * [MOBILE-1050] Clean up deploy
    
    * Add dev readme

---
+++ DEV_README.md
@@ -0,0 +1,24 @@
+# iOS Developer Readme
+
+Notes for the Airship Mobile team.
+
+## Deploying
+
+The release deploy will trigger automatically when pushing a version (#.#.#) tag to ios-library.
+It will automatically do the following:
+ - CI & Build SDK
+ - Deploy the Airship.zip (xcframeworks) and Airship.framework.zip (carthage) to Github as a new Release
+ - Deploy the Airship.zip (xcframeworks) to Bintray
+ - Publish Pods
+ - Upload the documentation to a GCS bucket that extdocs will pull from when it deploys Airship docs
+
+### Deploy Secrets
+
+Secrets needed to be setup for the Github Action to run:
+- BINTRAY_AUTH:  <username>:<apiKey>
+- COCOAPODS_TRUNK_TOKEN: The password in ~/.netrc for trunk.cocoapods.org after you authenticate with Cocoapods
+- SLACK_WEBHOOK: Webhook to slack build updates.
+
+
+
+
--- scripts/deploy_bintray.sh
+++ scripts/deploy_bintray.sh
@@ -9,7 +9,7 @@ VERSION=$(awk <$ROOT_PATH/Airship/AirshipConfig.xcconfig "\$1 == \"CURRENT_PROJE
 upload() {
   echo -e "Uploading $1 into ${VERSION}"
   curl -T $1 -H "X-Bintray-Package:urbanairship-sdk" -H "X-Bintray-Version:${VERSION}" \
-    -H "X-Bintray-Publish:0" -u $BINTRAY_API_KEY https://api.bintray.com/content/urbanairship/iOSTest/urbanairship-sdk/${VERSION}/
+    -H "X-Bintray-Publish:0" -u $BINTRAY_AUTH https://api.bintray.com/content/urbanairship/iOS/urbanairship-sdk/${VERSION}/
 }
 
 # Upload to bintray
@@ -17,13 +17,13 @@ upload "$ROOT_PATH/build/Airship.zip"
 
 # Fix version release date and description
 VERSION_PATCH="{ \"desc\": \"Urban Airship SDK for iOS\", \"released\": \"$(date -u +%Y-%m-%dT%H:%M:%S.000Z)\"}"
-curl --request PATCH -H "Content-Type: application/json" -u $BINTRAY_API_KEY --data "$VERSION_PATCH" https://api.bintray.com/packages/urbanairship/iOSTest/urbanairship-sdk/versions/${VERSION}
+curl --request PATCH -H "Content-Type: application/json" -u $BINTRAY_AUTH --data "$VERSION_PATCH" https://api.bintray.com/packages/urbanairship/iOS/urbanairship-sdk/versions/${VERSION}
 
 # Publish
-curl -X POST -u $BINTRAY_API_KEY https://api.bintray.com/content/urbanairship/iOSTest/urbanairship-sdk/${VERSION}/publish
+curl -X POST -u $BINTRAY_AUTH https://api.bintray.com/content/urbanairship/iOS/urbanairship-sdk/${VERSION}/publish
 
 # Needed or show in download list fails because bintray thinks the release is not published yet
 sleep 30
 
 # Show in download list
-curl -X PUT -H "Content-Type: application/json" -u $BINTRAY_API_KEY --data '{"list_in_downloads":true}' https://api.bintray.com/file_metadata/urbanairship/iOSTest/urbanairship-sdk/${VERSION}/Airship.zip
+curl -X PUT -H "Content-Type: application/json" -u $BINTRAY_AUTH --data '{"list_in_downloads":true}' https://api.bintray.com/file_metadata/urbanairship/iOS/urbanairship-sdk/${VERSION}/Airship.zip
