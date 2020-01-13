1d0d0e5bca989279c7fb55cfc3b356b78183ca84
Ognjen Ristanović <ognjen.ristanovic@urbanairship.com>
2020-01-13 19:26:53 +0100

    add internal acc classes (#1892)

---
+++ Airship/AirshipAccengage/Source/AccengageInternal/ACCRequest.h
@@ -0,0 +1,11 @@
+/* Copyright Airship and Contributors */
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ACCRequest : NSObject <NSCoding>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Airship/AirshipAccengage/Source/AccengageInternal/ACCRequest.m
@@ -0,0 +1,16 @@
+//* Copyright Airship and Contributors */
+
+#import "ACCRequest.h"
+
+@implementation ACCRequest
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)coder {
+    self = [super init];
+    return self;
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+    
+}
+
+@end
