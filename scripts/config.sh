667752d65bfc59e525396ad75af6e52e24541e2f
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2020-04-17 15:08:42 -0700

    [MOBILE-1496] Update build tools (#1948)
    * [MOBILE-1496] Update build tools
    
    * Pod install

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-XCODE_VERSION=11.3.1
+XCODE_VERSION=11.4
 
 TEST_DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 11'
 TEST_DESTINATION_TVOS='platform=tvOS Simulator,OS=latest,name=Apple TV'
