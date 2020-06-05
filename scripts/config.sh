97a38ebce1e56b28bd7e97f71e18f1486151ec2b
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2020-06-05 15:08:34 -0700

    [MOBILE-1667] Update latest build tools (#1977)

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-XCODE_VERSION=11.4.1
+XCODE_VERSION=11.5
 
 TEST_DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 11'
 TEST_DESTINATION_TVOS='platform=tvOS Simulator,OS=latest,name=Apple TV'
