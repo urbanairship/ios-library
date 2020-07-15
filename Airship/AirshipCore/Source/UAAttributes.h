6e6d8d699b2e1dd47207ceea83059e54d013257d
Benjamin Benoit <>
2020-07-15 16:36:01 +0200

    UAAttributes

---
+++ Airship/AirshipCore/Source/UAAttributes.h
@@ -0,0 +1,106 @@
+/* Copyright Airship and Contributors */
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/**
+* Title attribute.
+*/
+extern NSString * const title;
+
+/**
+* First name attribute.
+*/
+extern NSString * const first_name;
+
+/**
+* Last name attribute.
+*/
+extern NSString * const last_name;
+
+/**
+* Full name attribute.
+*/
+extern NSString * const full_name;
+
+/**
+* Gender attribute.
+*/
+extern NSString * const gender;
+
+/**
+* Zip code attribute.
+*/
+extern NSNumber * const zipcode;
+
+/**
+* City attribute.
+*/
+extern NSString * const city;
+
+/**
+* Region attribute.
+*/
+extern NSString * const region;
+
+/**
+* Country attribute.
+*/
+extern NSString * const country;
+
+/**
+* Birthdate attribute.
+*/
+extern NSDate * const birthdate;
+
+/**
+* Age attribute.
+*/
+extern NSNumber * const age;
+
+/**
+* Mobile phone attribute.
+*/
+extern NSNumber * const mobile_phone;
+
+/**
+* Home phone attribute.
+*/
+extern NSNumber * const home_phone;
+
+/**
+* Work phone attribute.
+*/
+extern NSNumber * const work_phone;
+
+/**
+* Loyalty tier attribute.
+*/
+extern NSString * const loyalty_tier;
+
+/**
+* Company attribute.
+*/
+extern NSString * const company;
+
+/**
+* Username attribute.
+*/
+extern NSString * const username;
+
+/**
+* Account creation attribute.
+*/
+extern NSDate * const account_creation;
+
+/**
+* Email attribute.
+*/
+extern NSString * const email;
+
+/**
+* Advertising id attribute.
+*/
+extern NSString * const advertising_id;
---
+++ Airship/AirshipCore/Source/UAAttributes.m
@@ -0,0 +1,29 @@
+// UALib
+#import "UAirship.h"
+
+/**
+* Predefined attributes.
+*/
+
+@implementation UAAttributes : NSObject
+
+NSString * const UAAttributeTitle = @"title";
+NSString * const UAAttributeFirstName = @"first_name";
+NSString * const UAAttributeLastName = @"last_name";
+NSString * const UAAttributeFullName = @"full_name";
+NSString * const UAAttributeGender = @"gender";
+NSNumber * const UAAttributeZipCode = nil;
+NSString * const UAAttributeCity = @"city";
+NSString * const UAAttributeRegion = @"region";
+NSString * const UAAttributeCountry = @"country";
+NSDate * const UAAttributeBirthdate = nil;
+NSNumber * const UAAttributeAge = nil;
+NSNumber * const UAAttributeMobilePhone = nil;
+NSNumber * const UAAttributeHomePhone = nil;
+NSNumber * const UAAttributeWorkPhone = nil;
+NSString * const UAAttributeLoyaltyTier = @"loyalty_tier";
+NSString * const UAAttributeCompany = @"company";
+NSString * const UAAttributeUsername = @"username";
+NSDate * const UAAttributeAccountCreation = nil;
+NSString * const UAAttributeEmail = @"email";
+NSString * const UAAttributeAdvertisingId = @"advertising_id";
