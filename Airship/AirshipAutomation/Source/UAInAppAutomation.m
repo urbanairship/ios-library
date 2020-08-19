d0bb363e5519057b73d49c8012d733a97ec6a0ff
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2020-08-19 09:56:17 -0700

    [MOBILE-1839] Up schedule limit to 1k (#2039)

--- Airship/AirshipAutomation/Source/UAInAppAutomation.m
+++ Airship/AirshipAutomation/Source/UAInAppAutomation.m
@@ -18,7 +18,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-static NSTimeInterval const MaxSchedules = 200;
+static NSTimeInterval const MaxSchedules = 1000;
 
 NSString *const UAInAppMessageManagerEnabledKey = @"UAInAppMessageManagerEnabled";
 NSString *const UAInAppMessageManagerPausedKey = @"UAInAppMessageManagerPaused";
