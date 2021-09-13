72aaa36e037cc42110be0e6ee45383299d47fe52
Marc Sciglimpaglia <marc@urbanairship.com>
2021-09-12 21:20:14 -0700

    [fix-accengage-xcframework] fix module import (#2339)

--- Airship/AirshipAccengage/Source/AirshipAccengage.h
+++ Airship/AirshipAccengage/Source/AirshipAccengage.h
@@ -12,7 +12,7 @@ FOUNDATION_EXPORT double AirshipAccengageVersionNumber;
 //! Project version string for AirshipAccengage.
 FOUNDATION_EXPORT const unsigned char AirshipAccengageVersionString[];
 
-#import "UAAccengageModuleLoader.h"
+#import "UAAccengageSDKModule.h"
 #import "ACCDeviceInformationSet.h"
 #import "ACCUserProfile.h"
 #import "Accengage.h"
