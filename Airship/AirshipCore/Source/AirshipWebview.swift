5b279b6f38f6304453d312b9f2cc2e546e582fd7
Ognjen Ristanović <ognjen.ristanovic@urbanairship.com>
2021-12-02 05:28:11 +0100

    update web view (#2409)
    Co-authored-by: Ognjen Ristanovic <ognjen.ristanovic@airship.com>

--- Airship/AirshipCore/Source/AirshipWebview.swift
+++ Airship/AirshipCore/Source/AirshipWebview.swift
@@ -7,6 +7,11 @@ import SwiftUI
 import Combine
 import WebKit
 
+@available(iOS 13.0.0, *)
+class WebViewState: ObservableObject {
+    var isLoading = PassthroughSubject<Bool, Never>()
+}
+
 /// Airship Webview
 @available(iOS 13.0.0, *)
 struct AirshipWebView : View {
@@ -14,10 +19,25 @@ struct AirshipWebView : View {
     let model: WebViewModel
 
     let constraints: ViewConstraints
-
+    
+    @ObservedObject var webViewState = WebViewState()
+    @State var isLoading = false
+   
     var body: some View {
-        WebViewView(request: URLRequest(url: URL(string: model.url)!))
-            .constraints(constraints)
+        ZStack {
+            WebViewView(request: URLRequest(url: URL(string: model.url)!), webViewState: webViewState)
+                .constraints(constraints)
+                .onReceive(self.webViewState.isLoading.receive(on: RunLoop.main)) { value in
+                    self.isLoading = value
+                }
+            if (self.isLoading) {
+                if #available(iOS 14.0.0,  *) {
+                    ProgressView()
+                } else {
+                    Loader(isAnimating: true)
+                }
+            }
+        }        
     }
 }
 
@@ -29,8 +49,13 @@ struct WebViewView : UIViewRepresentable {
     
     let request: URLRequest
     
+    @ObservedObject var webViewState: WebViewState
+    
     func makeUIView(context: Context) -> WKWebView  {
-        return WKWebView()
+        let webView = WKWebView()
+        context.coordinator.webView = webView
+        webView.navigationDelegate = context.coordinator
+        return webView
     }
         
     func updateUIView(_ uiView: WKWebView, context: Context) {
@@ -40,17 +65,51 @@ struct WebViewView : UIViewRepresentable {
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
+    
+    class Coordinator : NSObject, WKNavigationDelegate {
+           
+        var webViewView: WebViewView
+        var webView : WKWebView?
+            
+        init(_ uiWebView: WebViewView) {
+            self.webViewView = uiWebView
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            self.webViewView.webViewState.isLoading.send(false)
+        }
+                
+        func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+            self.webViewView.webViewState.isLoading.send(false)
+        }
+                
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            self.webViewView.webViewState.isLoading.send(false)
+            self.webView!.reload()
+        }
+                
+        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+            self.webViewView.webViewState.isLoading.send(true)
+        }
+    }
+
 }
 
+/// Loader
 @available(iOS 13.0.0, *)
-class Coordinator : NSObject, WKNavigationDelegate {
-       
-    var webView: WebViewView
-        
-    init(_ uiWebView: WebViewView) {
-        self.webView = uiWebView
+struct Loader: UIViewRepresentable {
+    
+    typealias UIView = UIActivityIndicatorView
+    var isAnimating: Bool
+ 
+    fileprivate var configuration = { (indicator: UIView) in }
+
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIView { UIView() }
+ 
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<Self>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+        configuration(uiView)
     }
 }
-
 
 #endif
