d900af400eff8a184d817837e9a0177a26d370b6
Ognjen Ristanović <ognjen.ristanovic@urbanairship.com>
2022-02-04 17:34:12 +0100

    wrap wrap (#2479)
    Co-authored-by: Ognjen Ristanovic <ognjen.ristanovic@airship.com>

--- Airship/AirshipPreferenceCenter/Source/PreferenceCenterCheckboxCell.swift
+++ Airship/AirshipPreferenceCenter/Source/PreferenceCenterCheckboxCell.swift
@@ -12,28 +12,40 @@ open class PreferenceCenterCheckboxCell: UITableViewCell {
     var callback : ((Bool, [ChannelScope])->())?
     var activeScopes : [ChannelScope] = []
     var contentStackView = UIStackView()
+    var contentWidth = 0.0
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-
+        contentWidth = contentView.frame.size.width
     }
 
     func draw(item : ContactSubscriptionGroupItem, style: PreferenceCenterStyle?) {
         clear()
         
-        let checkBoxWidgetView = UIView(frame: contentView.frame)
-
+        let spacing = 5
+        
         let cellStackView = UIStackView()
-        cellStackView.axis = .horizontal
+        cellStackView.axis = .vertical
         cellStackView.distribution = .fill
         cellStackView.alignment = .fill
-        cellStackView.spacing = 8
+        cellStackView.spacing = CGFloat(spacing * 2)
         cellStackView.translatesAutoresizingMaskIntoConstraints = false
         
         let count = item.components.count
+        var stackViewArray: [UIStackView] = []
+       
+        let rowStackView = UIStackView()
+        rowStackView.axis = .horizontal
+        rowStackView.distribution = .fill
+        rowStackView.alignment = .fill
+        rowStackView.spacing = CGFloat(spacing * 3)
+        rowStackView.translatesAutoresizingMaskIntoConstraints = false
+        stackViewArray.append(rowStackView)
+        
+        var contentSize = spacing * 2
 
         for i in 0...count-1 {
-          
+            
             let checkBox = PreferenceCenterCheckBox()
             checkBox.callback = callback
             let checkBoxLabel = UILabel()
@@ -41,6 +53,8 @@ open class PreferenceCenterCheckboxCell: UITableViewCell {
             let component = item.components[i]
             checkBoxLabel.text = component.display.title
             checkBoxLabel.font = style?.preferenceTextFont
+            let itemSize = (Int(checkBoxLabel.intrinsicContentSize.width) + Int(checkBox.intrinsicContentSize.width) + spacing * 4)
+            contentSize += itemSize
                 
             let scopes = component.scopes.values.reduce(into: []) { list, scope in
                 list.append(scope)
@@ -59,7 +73,7 @@ open class PreferenceCenterCheckboxCell: UITableViewCell {
             stackView.distribution = .fill
             stackView.alignment = .fill
             stackView.layer.cornerRadius = 18
-            stackView.spacing = 5
+            stackView.spacing = CGFloat(spacing)
             if #available(iOS 13.0, *) {
                 stackView.layer.borderColor = UIColor.opaqueSeparator.cgColor
             } else {
@@ -68,12 +82,35 @@ open class PreferenceCenterCheckboxCell: UITableViewCell {
             stackView.layer.borderWidth = 1
             stackView.layer.masksToBounds = true
             stackView.isLayoutMarginsRelativeArrangement = true
-            stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 5, leading: 7.5, bottom: 5, trailing: 7.5)
+            stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: CGFloat(spacing), leading: CGFloat(spacing * 2), bottom: CGFloat(spacing), trailing: CGFloat(spacing * 2))
 
-            cellStackView.addArrangedSubview(stackView)
+            if (contentSize >= Int(contentWidth)) {
+                let rowStackView = UIStackView()
+                rowStackView.axis = .horizontal
+                rowStackView.distribution = .fill
+                rowStackView.alignment = .fill
+                rowStackView.spacing = CGFloat(spacing * 3)
+                rowStackView.translatesAutoresizingMaskIntoConstraints = false
+                stackViewArray.append(rowStackView)
+                contentSize = itemSize + (spacing * 2)
+            }
+                       
+            let currentStackView = stackViewArray.last
+            if let currentStackView = currentStackView {
+                currentStackView.addArrangedSubview(stackView)
+            }
         }
 
-        checkBoxWidgetView.addSubview(cellStackView)
+        for stackView in stackViewArray {
+            let view = UIView(frame: contentView.frame)
+            view.addSubview(stackView)
+
+            stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+             
+            cellStackView.addArrangedSubview(view)
+        }
         
         let titleLabel = UILabel()
         let subtitleLabel = UILabel()
@@ -91,16 +128,16 @@ open class PreferenceCenterCheckboxCell: UITableViewCell {
         }
 
         if (item.display?.subtitle != nil && item.display?.subtitle != "") {
-            contentStackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel, checkBoxWidgetView])
+            contentStackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel, cellStackView])
         } else {
-            contentStackView = UIStackView(arrangedSubviews: [titleLabel, checkBoxWidgetView])
+            contentStackView = UIStackView(arrangedSubviews: [titleLabel, cellStackView])
         }
         contentStackView.axis = .vertical
-        contentStackView.distribution = .fillEqually
+        contentStackView.distribution = .fill
         contentStackView.alignment = .fill
-        contentStackView.spacing = 5
+        contentStackView.spacing = CGFloat(spacing)
         contentStackView.isLayoutMarginsRelativeArrangement = true
-        contentStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 5, leading: 15, bottom: 30, trailing: 0)
+        contentStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: CGFloat(spacing), leading: CGFloat(spacing) * 3, bottom: CGFloat(spacing) * 3, trailing: CGFloat(spacing))
 
         contentStackView.translatesAutoresizingMaskIntoConstraints = false
 
