8664c09b3df4b524365e8ac38fee3b818c71c447
Ryan Lepinski <ryanlepinski@Ryans-MacBook-Pro.local>
2022-10-05 13:32:06 -0700

    Fix missing 16.1 check

--- Airship/AirshipCore/Source/LiveActivityRestorer.swift
+++ Airship/AirshipCore/Source/LiveActivityRestorer.swift
@@ -2,6 +2,7 @@
 import ActivityKit
 
 /// Restores live activity tracking
+@available(iOS 16.1, *)
 public protocol LiveActivityRestorer {
     /// Should be called for every live activity type that you track with Airship.
     /// This method will check if any previously tracked live activities are still available by comparing
