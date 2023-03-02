b336e8f084bfa9888f40f8b17e22fcd22168e240
Ryan Lepinski <ryan.lepinski@airship.com>
2023-03-02 10:29:35 -0800

    Make text scale on Scenes & Surveys (#2689)

--- Airship/AirshipCore/Source/TextInput.swift
+++ Airship/AirshipCore/Source/TextInput.swift
@@ -9,6 +9,8 @@ struct TextInput: View {
     let model: TextInputModel
     let constraints: ViewConstraints
 
+    @Environment(\.sizeCategory) var sizeCategory
+
     @EnvironmentObject var formState: FormState
     @EnvironmentObject var thomasEnvironment: ThomasEnvironment
 
@@ -135,11 +137,12 @@ internal struct AirshipTextView: UIViewRepresentable {
         textView.inputAccessoryView = toolbar
         #endif
 
-        return textView.textAppearance(self.textAppearance, colorScheme)
+        textView.applyTextAppearance(self.textAppearance, colorScheme)
+        return textView
     }
 
     func updateUIView(_ uiView: UITextView, context: Context) {
-        uiView.textModifyAppearance(self.textAppearance)
+        uiView.applyTextAppearance(self.textAppearance, colorScheme)
         if uiView.text.isEmpty && !self.text.isEmpty {
             uiView.text = self.text
         }
@@ -180,4 +183,105 @@ internal struct AirshipTextView: UIViewRepresentable {
         }
     }
 }
+
+
+extension UITextView {
+    func applyTextAppearance<Appearance: BaseTextAppearance>(
+        _ textAppearance: Appearance?,
+        _ colorScheme: ColorScheme
+    ) {
+        if let textAppearance = textAppearance {
+            self.textAlignment =
+                textAppearance.alignment?.toNSTextAlignment() ?? .center
+            self.textColor =
+                textAppearance.color.toUIColor(colorScheme)
+            self.font = resolveUIFont(textAppearance)
+        }
+    }
+
+    func textModifyAppearance<Appearance: BaseTextAppearance>(
+        _ textAppearance: Appearance?
+    ) {
+        underlineText(textAppearance)
+    }
+
+    func underlineText<Appearance: BaseTextAppearance>(
+        _ textAppearance: Appearance?
+    ) {
+        if let textAppearance = textAppearance {
+            if let styles = textAppearance.styles {
+                if styles.contains(.underlined) {
+                    let textRange = NSRange(
+                        location: 0,
+                        length: self.text.count
+                    )
+                    let attributeString = NSMutableAttributedString(
+                        attributedString:
+                            self.attributedText
+                    )
+                    attributeString.addAttribute(
+                        .underlineStyle,
+                        value: NSUnderlineStyle.single.rawValue,
+                        range: textRange
+                    )
+                    self.attributedText = attributeString
+                }
+            }
+        }
+    }
+
+    private func resolveUIFont<Appearance: BaseTextAppearance>(
+        _ textAppearance: Appearance
+    ) -> UIFont {
+        var font = UIFont()
+        let scaledSize = UIFontMetrics.default.scaledValue(for: textAppearance.fontSize)
+
+        if let fontFamily = resolveFontFamily(
+            families: textAppearance.fontFamilies
+        ) {
+            font =
+                UIFont(
+                    name: fontFamily,
+                    size: scaledSize
+                )
+                ?? UIFont()
+        } else {
+            font = UIFont.systemFont(
+                ofSize: scaledSize
+            )
+        }
+
+        if let styles = textAppearance.styles {
+            if styles.contains(.bold) {
+                font = font.bold()
+            }
+            if styles.contains(.italic) {
+                font = font.italic()
+            }
+        }
+        return font
+    }
+
+    func resolveFontFamily(families: [String]?) -> String? {
+        if let families = families {
+            for family in families {
+                let lowerCased = family.lowercased()
+
+                switch lowerCased {
+                case "serif":
+                    return "Times New Roman"
+                case "sans-serif":
+                    return nil
+                default:
+                    if !UIFont.fontNames(forFamilyName: lowerCased).isEmpty {
+                        return family
+                    }
+                }
+            }
+        }
+        return nil
+    }
+}
 #endif
+
+
