306874ee0eda820b568ad5a25835d331cda0a5f8
Ulrich Giberné <ulrich.giberne@airship.com>
2023-04-07 17:46:11 -0400

    [MOBILE-3569] Handle autoplay for youtube videos (#2710)
    * handle autoplay on youtube video links
    
    * trying to fix the autoplaying when switching page
    
    * remove file
    
    * hacky fix autoplay
    
    * handle autoplay manually for both players
    
    * use nullable String and use playsinline if videoID is null

--- Airship/AirshipCore/Source/MediaWebView.swift
+++ Airship/AirshipCore/Source/MediaWebView.swift
@@ -14,15 +14,22 @@ struct MediaWebView: UIViewRepresentable {
     let type: MediaType
     let accessibilityLabel: String?
     let video: Video?
+    @Environment(\.isVisible) var isVisible
+    @State private var isLoaded: Bool = false
     
     func makeUIView(context: Context) -> WKWebView {
-        return createWebView()
+        return createWebView(context: context)
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
+        if isLoaded {
+            handleAutoplayingVideos(uiView: uiView)
+        } else {
+            pauseMedias(uiView: uiView)
+        }
     }
 
-    func createWebView() -> WKWebView {
+    func createWebView(context: Context) -> WKWebView {
         let config = WKWebViewConfiguration()
         config.allowsInlineMediaPlayback = true
         config.allowsPictureInPictureMediaPlayback = true
@@ -33,9 +40,18 @@ struct MediaWebView: UIViewRepresentable {
         webView.scrollView.isScrollEnabled = false
         webView.backgroundColor = UIColor.black
         webView.scrollView.backgroundColor = UIColor.black
+        webView.navigationDelegate = context.coordinator
         if type == .video {
             let html = String(
-                format: "<body style=\"margin:0\"><video playsinline %@ %@ %@ %@ height=\"100%%\" width=\"100%%\" src=\"%@\"></video></body>",
+                format: """
+                    <body style="margin:0">
+                        <video id="video" playsinline %@ %@ %@ %@ height="100%%" width="100%%" src="%@"></video>
+                        
+                        <script>
+                            let videoElement = document.getElementById("video");
+                        </script>
+                    </body>
+                    """,
                 video?.showControls ?? true ? "controls" : "",
                 video?.autoplay ?? false ? "autoplay" : "",
                 video?.muted ?? false ? "muted" : "",
@@ -45,15 +61,119 @@ struct MediaWebView: UIViewRepresentable {
             guard let mediaUrl = URL(string: url) else { return webView }
             webView.loadHTMLString(html, baseURL: mediaUrl)
         } else if type == .youtube {
-            guard
-                let url = URL(
-                    string: String(format: "%@%@", url, "?playsinline=1")
+            if let videoID = retrieveVideoID(url: url) {
+                let html = String(
+                    format: """
+                    <body style="margin:0">
+                        <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+                        <div id="player"></div>
+                    
+                        <script>
+                          // 2. This code loads the IFrame Player API code asynchronously.
+                          var tag = document.createElement('script');
+                    
+                          tag.src = "https://www.youtube.com/iframe_api";
+                          var firstScriptTag = document.getElementsByTagName('script')[0];
+                          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                    
+                          // 3. This function creates an <iframe> (and YouTube player)
+                          //    after the API code downloads.
+                          var player;
+                          function onYouTubeIframeAPIReady() {
+                            player = new YT.Player('player', {
+                              height: '100%%',
+                              width: '100%%',
+                              videoId: '%@',
+                              playerVars: {
+                                'playsinline': 1,
+                                'modestbranding': 1,
+                                'controls': %@,
+                                'autoplay': %@,
+                                'mute': %@,
+                                'loop': %@
+                              }
+                            });
+                          }
+                        </script>
+                    </body>
+                    """,
+                    videoID,
+                    video?.showControls ?? true ? "1" : "0",
+                    video?.autoplay ?? false ? "1" : "0",
+                    video?.muted ?? false ? "1" : "0",
+                    video?.loop ?? false ? "1, \'playlist\': \'\(videoID)\'" : "0"
                 )
-            else { return webView }
-            let request = URLRequest(url: url)
-            webView.load(request)
+                guard let mediaUrl = URL(string: url) else { return webView }
+                webView.loadHTMLString(html, baseURL: mediaUrl)
+            } else {
+                guard let videoUrl = URL(string: String(format: "%@%@", url, "?playsinline=1")) else {
+                    return webView
+                }
+                webView.load(URLRequest(url: videoUrl))
+            }
         }
         return webView
+    }
+    
+    func retrieveVideoID(url: String) -> String? {
+        do {
+            let regex = try NSRegularExpression(pattern: "embed/([a-zA-Z0-9_-]+).*")
+            let results = regex.firstMatch(in: url, range: NSRange(url.startIndex..., in: url))
+            let result = results.map {
+                String(url[Range($0.range, in: url)!])
+            }
+            guard let result = result else {
+                return nil
+            }
+            let separatedResult = result.components(separatedBy: "/")
+            return separatedResult[1]
+        } catch _ {
+            return nil
+        }
+    }
+    
+    func handleAutoplayingVideos(uiView: WKWebView) {
+        if isVisible {
+            if video?.autoplay ?? false {
+                playMedias(uiView: uiView)
+            }
+        } else {
+            pauseMedias(uiView: uiView)
+        }
+    }
+    
+    func pauseMedias(uiView: WKWebView) {
+        if type == .video {
+            uiView.evaluateJavaScript("videoElement.pause();")
+        } else if type == .youtube {
+            uiView.evaluateJavaScript("player.pauseVideo();")
+        }
+    }
+    
+    func playMedias(uiView: WKWebView) {
+        if type == .video {
+            uiView.evaluateJavaScript("videoElement.play();")
+        } else if type == .youtube {
+            uiView.evaluateJavaScript("player.playVideo();")
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self, isLoaded: $isLoaded)
+    }
+        
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: MediaWebView
+        var isLoaded: Binding<Bool>
+
+        init(_ parent: MediaWebView, isLoaded: Binding<Bool>) {
+            self.parent = parent
+            self.isLoaded = isLoaded
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            isLoaded.wrappedValue = true
+        }
     }
 }
 
