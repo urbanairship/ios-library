b3a0c61f06ba89a400ac35be2428401140390132
Ryan Lepinski <ryan.lepinski@airship.com>
2023-12-20 08:28:11 -0800

    [MOBILE-4052] Convert InAppMessageNativeBridgeExtension to swift (#2898)

---
+++ Airship/AirshipAutomationSwift/Source/InAppMessage/View/InAppMessageNativeBridgeExtension.swift
@@ -0,0 +1,37 @@
+/* Copyright Airship and Contributors */
+
+import Foundation
+import WebKit
+
+#if canImport(AirshipCore)
+import AirshipCore
+#endif
+
+/// Airship native bridge extension for an InAppMessage
+public class InAppMessageNativeBridgeExtension: NativeBridgeExtensionDelegate {
+
+    private let message: InAppMessage
+
+    public init(message: InAppMessage) {
+        self.message = message
+    }
+
+    public func actionsMetadata(
+        for command: JavaScriptCommand,
+        webView: WKWebView
+    ) -> [String: String] {
+        return [:]
+    }
+
+    public func extendJavaScriptEnvironment(
+        _ js: JavaScriptEnvironmentProtocol,
+        webView: WKWebView
+    ) async {
+        let extras = message.extras?.unWrap() as? [String : AnyHashable]
+
+        js.add(
+            "getMessageExtras",
+            dictionary: extras ?? [:]
+        )
+    }
+}
