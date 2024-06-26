a4401e1acd2be3bc28f3df9c16bd5d73222ec7c1
Ryan Lepinski <ryan.lepinski@airship.com>
2024-06-26 12:49:38 -0700

    [MOBILE-4563] Break up sleep into intervals (#3142)
    * [MOBILE-4563] Break up sleep into intervals
    
    * Add comment

--- Airship/AirshipCore/Source/AirshipTaskSleeper.swift
+++ Airship/AirshipCore/Source/AirshipTaskSleeper.swift
@@ -10,9 +10,32 @@ public protocol AirshipTaskSleeper: Sendable {
 /// NOTE: For internal use only. :nodoc:
 public final class DefaultAirshipTaskSleeper: AirshipTaskSleeper {
     fileprivate static let shared: DefaultAirshipTaskSleeper = DefaultAirshipTaskSleeper()
+    private static let maxDelayInterval: TimeInterval = 30
+
+    private let date: AirshipDateProtocol
+    private let onSleep: @Sendable (TimeInterval) async throws -> Void
+
+    init(
+        date: AirshipDateProtocol = AirshipDate.shared,
+        onSleep: @escaping @Sendable (TimeInterval) async throws -> Void = {
+            try await Task.sleep(nanoseconds: UInt64($0 * 1_000_000_000))
+        }
+    ) {
+        self.date = date
+        self.onSleep = onSleep
+    }
+
     public func sleep(timeInterval: TimeInterval) async throws {
-        if (timeInterval > 0) {
-            try await Task.sleep(nanoseconds: UInt64(timeInterval * 1_000_000_000))
+        let start: Date = date.now
+
+        /// Its unclear what clock is being used for Task.sleep(nanoseconds:) and we have had issues
+        /// with really long delays not firing at the right period of time. This works around those issues by
+        /// breaking long sleeps into chunks.
+        var remaining = timeInterval - date.now.timeIntervalSince(start)
+        while remaining > 0, !Task.isCancelled {
+            let interval = min(remaining, Self.maxDelayInterval)
+            try await onSleep(interval)
+            remaining = timeInterval - date.now.timeIntervalSince(start)
         }
     }
 }
