a9d0d1d5284767ff1ccef34026893b8fb22a54b4
Ryan Lepinski <ryan.lepinski@airship.com>
2024-12-18 20:25:51 -0800

    Fix version

--- Airship/AirshipCore/Source/AirshipVersion.swift
+++ Airship/AirshipCore/Source/AirshipVersion.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 public struct AirshipVersion {
-    public static let version = "18.4.0"
+    public static let version = "18.14.0"
     public static func get() -> String {
         return version
     }
