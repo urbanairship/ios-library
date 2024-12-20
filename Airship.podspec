ca8f02e137474bc82d75117ca0d0840ed151499a
Ryan Lepinski <ryan.lepinski@airship.com>
2024-12-20 11:20:33 -0800

    Bump version

--- Airship.podspec
+++ Airship.podspec
@@ -1,4 +1,4 @@
-AIRSHIP_VERSION="18.14.1"
+AIRSHIP_VERSION="19.0.0"
 
 Pod::Spec.new do |s|
    s.version                    = AIRSHIP_VERSION
--- Airship/AirshipCore/Source/AirshipVersion.swift
+++ Airship/AirshipCore/Source/AirshipVersion.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 public struct AirshipVersion {
-    public static let version = "18.14.1"
+    public static let version = "19.0.0"
     public static func get() -> String {
         return version
     }
