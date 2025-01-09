89003c30ec77ef5d8419e6725d1c673d64429825
Ryan Lepinski <ryan.lepinski@airship.com>
2025-01-09 10:02:18 -0800

    Release 18.14.2 (#3337)
    * Fix banner spacing without heading
    
    * Release 18.14.2

--- Airship/AirshipCore/Source/AirshipVersion.swift
+++ Airship/AirshipCore/Source/AirshipVersion.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 public struct AirshipVersion {
-    public static let version = "18.14.1"
+    public static let version = "18.14.2"
     public static func get() -> String {
         return version
     }
