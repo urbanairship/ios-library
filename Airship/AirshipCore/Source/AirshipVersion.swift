77e38ad14db32e80deee44f03578b7912c45829b
David <jdavidcrow@gmail.com>
2025-09-15 17:51:31 -0700

    Release 19.9.2 (#3535)
    Co-authored-by: crow <david.crow@airship.com>

--- Airship/AirshipCore/Source/AirshipVersion.swift
+++ Airship/AirshipCore/Source/AirshipVersion.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 public struct AirshipVersion {
-    public static let version = "19.9.1"
+    public static let version = "19.9.2"
     public static func get() -> String {
         return version
     }
