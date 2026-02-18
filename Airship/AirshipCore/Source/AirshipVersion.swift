d07367eb662441e3c8935bde552205b84860d52b
Ryan Lepinski <ryan.lepinski@airship.com>
2026-02-18 15:03:06 -0800

    Release 20.3.1 (#3679)

--- Airship/AirshipCore/Source/AirshipVersion.swift
+++ Airship/AirshipCore/Source/AirshipVersion.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 public struct AirshipVersion {
-    public static let version = "20.3.0"
+    public static let version = "20.3.1"
     public static func get() -> String {
         return version
     }
