203397bb3418bab274cb269ef7be7fcab8b98c77
Ryan Lepinski <ryan.lepinski@airship.com>
2026-02-27 11:52:39 -0800

    Window factory (#3698)

--- Airship/AirshipCore/Source/AirshipWindowFactory.swift
+++ Airship/AirshipCore/Source/AirshipWindowFactory.swift
@@ -1,30 +1,47 @@
 /* Copyright Airship and Contributors */
 
-#if canImport(UIKit) && !os(watchOS)
+#if !os(watchOS)
 
+#if canImport(AppKit)
+public import AppKit
+#elseif canImport(UIKit)
 public import UIKit
+#endif
 
 /// A singleton factory class to create and configure UIWindow objects.
 /// This allows apps to override behaviors like dark mode and other window-specific settings.
 @MainActor
-public final class AirshipWindowFactory {
+public final class AirshipWindowFactory: Sendable {
 
     /// The shared instance of `AirshipWindowFactory` for centralized window creation.
-    public static let shared: AirshipWindowFactory = AirshipWindowFactory()
-
-    /// A closure that allows apps to customize window creation.
-    /// If nil, the default window creation behavior will be used.
-    public var makeBlock: (@MainActor @Sendable (UIWindowScene) -> UIWindow)?
+    public static let shared = AirshipWindowFactory()
 
     private init() {}
+
+#if os(macOS)
+    /// A closure that allows apps to customize window creation.
+    public var makeBlock: (@MainActor @Sendable () -> NSWindow)?
+
+    /// Creates a new NSWindow.
+    public func makeWindow() -> NSWindow {
+        return makeBlock?() ?? NSWindow(
+            contentRect: .zero,
+            styleMask: [.titled, .closable, .resizable, .miniaturizable],
+            backing: .buffered,
+            defer: false
+        )
+    }
+#else
+    /// A closure that allows apps to customize window creation.
+    public var makeBlock: (@MainActor @Sendable (UIWindowScene) -> UIWindow)?
 
     /// Creates a new UIWindow for the given window scene.
     ///
     /// - Parameter windowScene: The `UIWindowScene` in which the new window will be created.
-    /// - Returns: A `UIWindow` object configured for the given `UIWindowScene`.
     public func makeWindow(windowScene: UIWindowScene) -> UIWindow {
         return makeBlock?(windowScene) ?? UIWindow(windowScene: windowScene)
     }
+#endif
 }
 
 #endif
