7a8b4a3dcf18b94e736c014b1a39b8ae08f9c92d
Ryan Lepinski <ryan.lepinski@airship.com>
2026-02-27 16:36:49 -0800

    Fix

--- Airship/AirshipCore/Source/AirshipNativeViewRepresentable.swift
+++ Airship/AirshipCore/Source/AirshipNativeViewRepresentable.swift
@@ -5,7 +5,7 @@ public import SwiftUI
 #if os(macOS)
 /// A platform-agnostic view representable that bridges to NSViewRepresentable on macOS.
 public typealias AirshipNativeViewRepresentable = NSViewRepresentable
-#else
+#elseif !os(watchOS)
 /// A platform-agnostic view representable that bridges to UIViewRepresentable on iOS, tvOS, and visionOS.
 public typealias AirshipNativeViewRepresentable = UIViewRepresentable
 #endif
